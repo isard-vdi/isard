2d4ed6f5575e0809b788360ed6245406d09e5cb8
darta <jvinolas@correu.escoladeltreball.org>
2017-05-06 23:11:54 +0200

    New classrom dict test

--- webapp/views/ClassroomViews.py
+++ webapp/views/ClassroomViews.py
@@ -20,14 +20,21 @@ db.init_app(app)
 
 from .decorators import ownsid
 
+import random
 @app.route('/classroom', methods=['POST','GET'])
 @login_required
 def classroom():
     if request.method == 'POST':
         None
-    hosts=[]
-    for i in range(1,30):
-        hosts.append({'ip':'10.200.210.'+str(i),'hostname':'f2a-'+str(i)})
+    hosts={}
+    
+    for r in range(1,6):
+        hosts[r]={}
+        for c in range(1,random.randrange(4,8)):
+            hosts[r][c]={'status':random.randrange(0,3),'ip':'10.200.210.'+str(r+c),'hostname':'f2a-'+str(r+c)}
+    
+
+#~ ':'f2a-'+str(i)})
     #~ hosts=[{'ip':'10.200.210.1','hostname':'f2a-01'},
             #~ {'ip':'10.200.210.2','hostname':'f2a-02'},
             #~ {'ip':'10.200.210.3','hostname':'f2a-03'},]
