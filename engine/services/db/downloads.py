1e087237e3850e14af645dff93537679afa29e63
alberto <alarraz@escoladeltreball.org>
2018-01-29 23:38:06 +0100

    engine delete media

--- engine/services/db/downloads.py
+++ engine/services/db/downloads.py
@@ -32,6 +32,21 @@ def update_status_table(table,status,id_media,detail=""):
     r.table(table).get(id_media).update(d).run(r_conn)
     close_rethink_connection(r_conn)
 
+def update_status_media_from_path(path,status,detail=''):
+    r_conn = new_rethink_connection()
+    table = 'media'
+    d_status ={'status':status,
+               'detail':detail}
+    result = []
+    l = list(r.table(table).filter({'path_downloaded': path}).run(r_conn))
+    if len(l) > 0:
+        for d in l:
+            result.append(r.table(table).get(d['id']).update(d_status).run(r_conn))
+    else:
+        result = False
+    close_rethink_connection(r_conn)
+    return result
+
 def update_download_percent(done,table,id):
     r_conn = new_rethink_connection()
     r.table(table).get(id).update({'progress': done}).run(r_conn)
