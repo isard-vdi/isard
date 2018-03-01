63e7fc7b3c36925b9dbf120c266f9f882dc74c5c
darta <vinolasbcn@gmail.com>
2018-03-01 12:30:45 +0100

    Added production gulp file

--- webapp/static/admin/js/graphs/tree.js
+++ webapp/static/admin/js/graphs/tree.js
@@ -196,10 +196,10 @@ function update(source) {
   // Creates a curved (diagonal) path from parent to the child nodes
   function diagonal(s, d) {
 
-    path = `M ${s.y} ${s.x}
-            C ${(s.y + d.y) / 2} ${s.x},
-              ${(s.y + d.y) / 2} ${d.x},
-              ${d.y} ${d.x}`
+    path = 'M ${s.y} ${s.x} \
+            C ${(s.y + d.y) / 2} ${s.x}, \
+              ${(s.y + d.y) / 2} ${d.x}, \
+              ${d.y} ${d.x}'
 
     return path
   }
