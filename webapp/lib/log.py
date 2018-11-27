17524e3d1108c8b4865433e65f980aab504033bd
root <root@isard.localhost>
2018-11-27 14:43:25 -0600

    Fixed role, category and grop modals

--- webapp/lib/log.py
+++ webapp/lib/log.py
@@ -11,27 +11,17 @@ import configparser
 import os
 from webapp import app
 
-
-
-#~ import logging as log
-#~ log.basicConfig(level=log.INFO)
-#~ logger = log.getLogger(__name__) 
-
-#~ try:
-    #~ rcfg = configparser.ConfigParser()
-    #~ rcfg.read(os.path.join(os.path.dirname(__file__),'../../isard.conf'))
-#~ except Exception as e:
-    #~ log.info('Aborting. Please configure your log level in: isard.conf \n exception: {}'.format(e))
-    #~ sys.exit(0)
-    
-#~ LOG_LEVEL = rcfg.get('LOG', 'LEVEL')
-#~ LOG_FILE  = rcfg.get('LOG', 'FILE')
 try:
     LOG_LEVEL = app.config['LOG_LEVEL']
 except Exception as e:
     LOG_LEVEL = 'INFO'
+    
 # LOG FORMATS
 LOG_FORMAT='%(asctime)s %(msecs)d - %(levelname)s - %(threadName)s: %(message)s'
 LOG_DATE_FORMAT='%Y/%m/%d %H:%M:%S'
 LOG_LEVEL_NUM = log.getLevelName(LOG_LEVEL)
-log.basicConfig(format=LOG_FORMAT,datefmt=LOG_DATE_FORMAT,level=LOG_LEVEL_NUM)
+# ~ log.basicConfig(format=LOG_FORMAT,datefmt=LOG_DATE_FORMAT,level=LOG_LEVEL_NUM)
+
+log.basicConfig(filename='logs/webapp.log',
+                            filemode='a',
+                            format=LOG_FORMAT,datefmt=LOG_DATE_FORMAT,level=LOG_LEVEL_NUM)
