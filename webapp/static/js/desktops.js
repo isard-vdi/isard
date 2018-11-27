7e14ab4fd2c97c036eb643a0010927fca2bde730
root <root@isard.localhost>
2018-11-27 14:59:14 -0600

    Activate delete desktop button in any state not Started

--- webapp/static/js/desktops.js
+++ webapp/static/js/desktops.js
@@ -394,6 +394,7 @@ function setDesktopDetailButtonsStatus(id,status){
           }
           if(status!='Started'){
               $('#actions-'+id+' .btn-edit').prop('disabled', false);
+              $('#actions-'+id+' .btn-delete').prop('disabled', false);
           }
            
 }
