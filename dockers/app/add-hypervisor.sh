0cc3af7626c3b18a0f451fd58880d40f1e61e126
darta <vinolasbcn@gmail.com>
2019-01-07 08:40:15 -0600

    Added new hyper and port and username

--- dockers/app/add-hypervisor.sh
+++ dockers/app/add-hypervisor.sh
@@ -1,9 +1,21 @@
 if [[ -z $HYPERVISOR || -z $PASSWORD ]]
 then
     echo "You should add environment variables:"
-    echo " docker exec -e HYPERVISOR=<IP|DNS> -e PASSWORD=<YOUR_ROOT_PASSWD> isard_isard-app_1 bash -c '/add-hypervisor.sh'"
+    echo "  docker exec -e HYPERVISOR=<IP|DNS> -e PASSWORD=<YOUR_ROOT_PASSWD> isard_isard-app_1 bash -c '/add-hypervisor.sh'"
+    echo "Optional parameters: USER (default is root), PORT (default is 22)"
+    echo ""
     echo "Please run it again setting environment variables"
     exit 1
+fi
+
+if [[ -z $PORT ]]
+then
+    PORT=22
+fi
+
+if [[ -z $USER ]]
+then
+    USER=root
 fi
 
 apk add sshpass
@@ -13,33 +25,33 @@ then
 fi
 sed -i '/'"$HYPERVISOR"'/d' /root/.ssh/known_hosts
 echo "Trying to ssh into $HYPERVISOR..."
-ssh-keyscan $HYPERVISOR > /NEWHYPER
+ssh-keyscan -p $PORT $HYPERVISOR > /NEWHYPER
 if [ ! -s /NEWHYPER ]
 then
-    echo "Hypervisor $HYPERVISOR could not be reached. Aborting"
+    echo "Hypervisor $HYPERVISOR:$PORT could not be reached. Aborting"
     exit 1
 else
     cat /NEWHYPER >> /root/.ssh/known_hosts
-    sshpass -p "$PASSWORD" ssh-copy-id root@"$HYPERVISOR"
+    sshpass -p "$PASSWORD" ssh-copy-id -p $PORT $USER@"$HYPERVISOR"
     if [ $? -ne 0 ]
     then
        sed -i '/'"$HYPERVISOR"'/d' /root/.ssh/known_hosts
-       echo "Can't access $HYPERVISOR as root user. Aborting"
+       echo "Can't access $USER@$HYPERVISOR:$PORT. Aborting"
        exit 1
     fi
 fi
 
 echo "Hypervisor ssh access granted."
-virsh -c qemu+ssh://"$HYPERVISOR"/system quit
+virsh -c qemu+ssh://"$USER"@"$HYPERVISOR":"$PORT"/system quit
 if [ $? -ne 0 ]
 then
-   echo "Can't access libvirtd daemon. Please ensure that libvirt daemon is running in $HYPERVISOR. Aborting"
+   echo "Can't access libvirtd daemon. Please ensure that libvirt daemon is running in $USER@$HYPERVISOR:$PORT. Aborting"
    sed -i '/'"$HYPERVISOR"'/d' /root/.ssh/known_hosts 
    exit 1
 fi
 
 
-echo "Access to $HYPERVISOR granted and found libvirtd service running."
+echo "Access to $USER@$HYPERVISOR:$PORT granted and found libvirtd service running."
 echo "Now you can create this hypervisor in IsardVDI web interface."
 
 
