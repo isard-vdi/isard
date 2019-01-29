6a02ca876398460ec1805c19c4385474dd325025
Josep Maria Viñolas <jvinolas@correu.escoladeltreball.org>
2019-01-29 11:29:44 +0100

    not updated

--- src/webapp/lib/admin_api.py
+++ src/webapp/lib/admin_api.py
@@ -334,26 +334,30 @@ class isardAdmin():
                 if not id:
                     return list(r.table("domains").get_all(r.args(['public_template','user_template']),index='kind').without('xml','history_domain').merge(lambda domain:
                         {
-                            "derivates": r.table('domains').filter({'create_dict':{'origin':domain['id']}}).count()
+                            "derivates": r.table('domains').filter(lambda derivates: derivates['parents'].contains(domain['id'])).count()
+                            # ~ "derivates": r.table('domains').filter({'create_dict':{'origin':domain['id']}}).count()
                         }
                     ).run(db.conn))
                 if id:
                     return list(r.table("domains").get(id).without('xml','history_domain').merge(lambda domain:
                         {
-                            "derivates": r.table('domains').filter({'create_dict':{'origin':domain['id']}}).count()
+                            "derivates": r.table('domains').filter(lambda derivates: derivates['parents'].contains(domain['id'])).count()
+                            # ~ "derivates": r.table('domains').filter({'create_dict':{'origin':domain['id']}}).count()
                         }
                     ).run(db.conn))
             elif kind == 'base':
                 if not id:
                     return list(r.table("domains").get_all(kind,index='kind').without('xml','history_domain').merge(lambda domain:
                         {
-                            "derivates": r.table('domains').filter({'create_dict':{'origin':domain['id']}}).count()
+                            "derivates": r.table('domains').filter(lambda derivates: derivates['parents'].contains(domain['id'])).count()
+                            # ~ "derivates": r.table('domains').filter({'create_dict':{'origin':domain['id']}}).count()
                         }
                     ).run(db.conn))
                 if id:
                     return list(r.table("domains").get(id).without('xml','history_domain').merge(lambda domain:
                         {
-                            "derivates": r.table('domains').filter({'create_dict':{'origin':domain['id']}}).count()
+                            "derivates": r.table('domains').filter(lambda derivates: derivates['parents'].contains(domain['id'])).count()
+                            # ~ "derivates": r.table('domains').filter({'create_dict':{'origin':domain['id']}}).count()
                         }
                     ).run(db.conn))                
             else:
@@ -1381,8 +1385,64 @@ class Certificates(object):
 
 
 
-        
-        
+'''
+TREE
+'''   
+# ~ from collections import defaultdict     
+# ~ class tree(object):
+    # ~ def __init__(self,id):
+        # ~ with 
+            
+            
+
+
+# ~ input_ = '''dir/file
+# ~ dir/dir2/file2
+# ~ dir/file3
+# ~ dir2/alpha/beta/gamma/delta
+# ~ dir2/alpha/beta/gamma/delta/
+# ~ dir3/file4
+# ~ dir3/file5'''
+
+# ~ FILE_MARKER = '<files>'
+
+    # ~ def attach(branch, trunk):
+        # ~ '''
+        # ~ Insert a branch of directories on its trunk.
+        # ~ '''
+        # ~ parts = branch.split('/', 1)
+        # ~ if len(parts) == 1:  # branch is a file
+            # ~ trunk[FILE_MARKER].append(parts[0])
+        # ~ else:
+            # ~ node, others = parts
+            # ~ if node not in trunk:
+                # ~ trunk[node] = defaultdict(dict, ((FILE_MARKER, []),))
+            # ~ attach(others, trunk[node])
+
+    # ~ def prettify(d, indent=0):
+        # ~ '''
+        # ~ Print the file tree structure with proper indentation.
+        # ~ '''
+        # ~ for key, value in d.iteritems():
+            # ~ if key == FILE_MARKER:
+                # ~ if value:
+                    # ~ print '  ' * indent + str(value)
+            # ~ else:
+                # ~ print '  ' * indent + str(key)
+                # ~ if isinstance(value, dict):
+                    # ~ prettify(value, indent+1)
+                # ~ else:
+                    # ~ print '  ' * (indent+1) + str(value)
+
+
+
+# ~ main_dict = defaultdict(dict, ((FILE_MARKER, []),))
+# ~ for line in input_.split('\n'):
+    # ~ attach(line, main_dict)
+
+# ~ prettify(main_dict) 
+
+       
 '''
 FLATTEN AND UNFLATTEN DICTS
 '''        
