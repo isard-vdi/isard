0853196cc982544336e4f0fd251750011cda0076
beto <alarraz@escoladeltreball.org>
2019-02-20 12:15:42 +0100

    bug in get_pool_from_domain

--- src/engine/services/db/domains.py
+++ src/engine/services/db/domains.py
@@ -400,24 +400,20 @@ def get_pool_from_domain(domain_id):
     r_conn = new_rethink_connection()
     rtable = r.table('domains')
     try:
-        d = rtable.get(domain_id).pluck('pool', 'kind').run(r_conn)
-        if d['kind'] == 'desktop':
-            if 'pool' not in d.keys():
-                pool = 'default'
+        d = rtable.get(domain_id).pluck('hypervisors_pools').run(r_conn)
+        if len(d) > 0:
+            if len(d['hypervisors_pools']) > 0:
+                pool = d['hypervisors_pools'][0]
             else:
-                if type(d['pool']) is unicode or type(d['pool']) is str:
-                    rtable = r.table('hypervisors_pools')
-                    d_pool = rtable.get(d['pool']).run(r_conn)
-                    if len(d_pool.keys()) > 0:
-                        pool = d['pool']
-                    else:
-                        pool = 'default'
+                log.error(f'domain: {domain_id} with not hypervisors_pools in list. Pool default forced.')
+                pool = 'default'
         else:
-            pool = False
+            log.error(f'domain: {domain_id} withouth hypervisors_pools key defined. Pool default forced.')
+            pool = 'default'
     except r.ReqlNonExistenceError:
         log.error('domain_id {} does not exist in domains table'.format(domain_id))
         log.debug('function: {}'.format(sys._getframe().f_code.co_name))
-        pool = False
+        pool = 'default'
 
     close_rethink_connection(r_conn)
     return pool
