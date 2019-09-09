c31437512e6884147b67d39c815b8a6896e0e7ad
jake <jake@jakecoffman.com>
2019-09-08 22:21:38 -0500

    this seems to work just fine

--- stream.go
+++ stream.go
@@ -1,10 +1,8 @@
 package guac
 
 import (
-	"fmt"
 	logger "github.com/sirupsen/logrus"
 	"net"
-	"sync"
 	"time"
 )
 
@@ -14,10 +12,8 @@ const (
 )
 
 type Stream struct {
-	lock    sync.RWMutex
 	conn    net.Conn
 	timeout time.Duration
-	err     error
 }
 
 // NewStream Construct function
@@ -28,90 +24,20 @@ func NewStream(conn net.Conn, timeout time.Duration) (ret *Stream) {
 	return
 }
 
-func (s *Stream) errClose(err error) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	if s.conn == nil {
-		return
-	}
-	_ = s.conn.Close()
-	s.conn = nil
-	s.err = err
-	logger.Debug("close socket")
-}
-
 func (s *Stream) Write(data []byte) (n int, err error) {
-	if s.err != nil {
-		err = s.err
+	if err = s.conn.SetWriteDeadline(time.Now().Add(s.timeout)); err != nil {
+		logger.Error(err)
 		return
 	}
-	if s.conn == nil {
-		err = net.ErrWriteToConnected
-		return
-	}
-	if s.timeout > 0 {
-		if err = s.conn.SetWriteDeadline(time.Now().Add(s.timeout)); err != nil {
-			s.errClose(err)
-			return
-		}
-	}
-	try := 0
-	pos := 0
-	for pos < len(data) {
-		n, err = s.conn.Write(data[pos:])
-		if err != nil {
-			if en, ok := err.(net.Error); !ok || !en.Temporary() || try >= 3 {
-				s.errClose(err)
-				return
-			}
-			try++
-			continue
-		}
-		pos += n
-	}
-	if s.timeout > 0 {
-		if err = s.conn.SetWriteDeadline(time.Time{}); err != nil {
-			s.errClose(err)
-			return
-		}
-	}
-	return
+	return s.conn.Write(data)
 }
 
 func (s *Stream) Read(p []byte) (n int, err error) {
-	if s.err != nil {
-		err = s.err
+	if err = s.conn.SetReadDeadline(time.Now().Add(s.timeout)); err != nil {
+		logger.Error(err)
 		return
 	}
-	if s.conn == nil {
-		err = net.ErrWriteToConnected
-		return
-	}
-	if s.timeout > 0 {
-		if err = s.conn.SetReadDeadline(time.Now().Add(s.timeout)); err != nil {
-			// s.errClose(err)
-			return
-		}
-	}
-	for try := 0; try < 3; try++ {
-		n, err = s.conn.Read(p)
-		if err != nil {
-			ex, ok := err.(net.Error)
-			if ok && ex.Temporary() {
-				continue
-			}
-			return
-		}
-		break
-	}
-
-	if s.timeout > 0 {
-		if err = s.conn.SetReadDeadline(time.Time{}); err != nil {
-			// s.errClose(err)
-			return
-		}
-	}
-	return
+	return s.conn.Read(p)
 }
 
 // Available stream check
@@ -123,6 +49,5 @@ func (s *Stream) Available() (bool, error) {
 
 // Close stream close
 func (s *Stream) Close() (err error) {
-	s.errClose(fmt.Errorf("closed"))
-	return
+	return s.conn.Close()
 }
