f2eca1251fcecc0f584071dfda9ee75007ace1e1
Josep Maria Viñolas <jvinolas@correu.escoladeltreball.org>
2020-05-15 18:24:53 +0200

    working

--- hyper/docker/run.sh
+++ hyper/docker/run.sh
@@ -1,2 +1,13 @@
+echo "Generating selfsigned certs for spice client..."
+sh auto-generate-certs.sh
+echo "Starting libvirt daemon..."
 /usr/sbin/virtlogd &
 /usr/sbin/libvirtd &
+sleep 5
+echo "Checking hypervisor..."
+echo "[1/1] basic domain start..."
+virsh create checks/domain.xml
+virsh destroy domain
+if [ $? -eq 0 ]; then
+	exec /bin/sh -c "trap : TERM INT; sleep infinity & wait"
+fi
