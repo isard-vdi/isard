7e2939c920dab7f36496523c10a8edee1706d160
darta <josepmaria@isardvdi.com>
2020-09-07 10:05:26 +0200

    removed sudo as missing in debian

--- sysadm/debian_docker.sh
+++ sysadm/debian_docker.sh
@@ -5,10 +5,9 @@ apt-get install -y \
     curl \
     gnupg-agent \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
-   #$(lsb_release -cs) \
    buster \
    stable"
 apt-get update -y
