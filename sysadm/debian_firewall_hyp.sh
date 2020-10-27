d3d2ba3a83fdda3c4ad0b48d172fade4ab7ffeec
darta <vinolasbcn@gmail.com>
2020-10-27 01:32:26 +0100

    Fixed bug in iptables 1.8
    Co-authored-by: Simó Albert i Beltran <sim6@bona.gent>

--- sysadm/debian_firewall_hyp.sh
+++ sysadm/debian_firewall_hyp.sh
@@ -1,5 +1,10 @@
 apt install firewalld fail2ban -y
 
+# Fixes bug in iptables 1.8
+echo "deb http://deb.debian.org/debian buster-backports main" > /etc/apt/sources.list.d/buster-backports.list
+apt update
+apt install -y iptables -t buster-backports
+
 #echo "Setting iptables to not use nf_tables"
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
