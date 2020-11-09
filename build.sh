5471b36015278ea81f942952940861864d923307
Simó Albert i Beltran <sim6@bona.gent>
2020-11-09 22:21:37 +0100

    Allow docker-compose pull without source code
    Restructure docker-compose files generation to simplify to adding more
    flavours and generate pull, build and devel variants.
    
    Also removes the docker-compose files, which will be available in
    gitlab-ci.
    
    https://gitlab.com/isard/isardvdi/-/jobs/artifacts/develop/raw/docker-compose.yml?job=docker-build

--- build.sh
+++ build.sh
@@ -1,156 +1,163 @@
-#!/bin/bash
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    echo "Pass parameters:"
-    echo "  without parameters will build everything: web+hyper with video as a docker-compose .yml"
-    echo "  <web> (only web, no hypevisor nor video)"
-    echo "  <hypervisor> (only hypervisor with video)"
-    echo "  <hypervisor-standalone> (only hypervisor without video)"
-    echo "  <video-standalone> (only video without hypervisor)"
-    exit 1
-fi
-
-read_var() {
-    VAR=$(grep $1 $2 | xargs)
-    IFS="=" read -ra VAR <<< "$VAR"
-    echo ${VAR[1]}
-}
-
+#!/bin/sh
 set -e
+
 git submodule init
 git submodule update --recursive --remote
 
 cp isardvdi.cfg .env
+## BUILD_ROOT_PATH env
+# This is a workarround for
+# https://github.com/docker/compose/issues/7873
+# See also BUILD_ROOT_PATH sed section at the end of file
 echo "BUILD_ROOT_PATH=$(pwd)" >> .env
-cp isardvdi.cfg ymls/.env
 
-HYPERVISOR_HOST_TRUNK_INTERFACE=$(read_var HYPERVISOR_HOST_TRUNK_INTERFACE .env)
-echo $HYPERVISOR_HOST_TRUNK_INTERFACE
-if [ -z "$HYPERVISOR_HOST_TRUNK_INTERFACE" ]; then
-    HYPER_YAMLS="-f ymls/isard-hypervisor.yml"
-else
-    HYPER_YAMLS="-f ymls/isard-hypervisor.yml -f ymls/isard-hypervisor-vlans.yml"
-fi
+. ./.env
+PARTS_PATH=docker-compose-parts
 
-docker-compose \
-    -f ymls/isard-stats.yml \
-    config > docker-compose.stats.yml
-docker-compose \
-    -f docker-compose.stats.yml \
-    -f ymls/devel/isard-stats.yml.devel \
-    config > docker-compose.stats.devel.yml
+parts_files(){
+	for part in $@
+	do
+		local file="$PARTS_PATH/$part.yml"
+		if [ -f "$file" ]
+		then
+			echo -n "-f $file "
+		fi
+	done
+}
+merge(){
+	local flavour="$1"
+	shift || return 0
+	local args="$(parts_files $@)"
+	if [ -n "$*" -a -n "$args" ]
+	then
+		if [ -z "$flavour" ]
+		then
+			local delimiter=""
+		else
+			local delimiter="."
+		fi
+		docker-compose $args config > "docker-compose$delimiter$flavour.yml"
+	fi
+}
+parts_variant(){
+	local variant="$1"
+	shift || return 0
+	for part in $@
+	do
+		echo -n "$part.$variant "
+	done
+}
+variants(){
+	local flavour="$1"
+	shift || return 0
+	if [ -z "$flavour" ]
+	then
+		local delimiter=""
+	else
+		local delimiter="."
+	fi
+	merge "$flavour" $@
+	merge "$flavour${delimiter}build" $@ $(parts_variant build $@)
+	merge "$flavour${delimiter}devel" $@ $(parts_variant build $@) $(parts_variant devel $@)
+}
+flavour(){
+	local flavour="$1"
+	shift || return 0
+	local no_stats_parts=""
+	local parts=""
+	for part in $@
+	do
+		parts="$parts $part"
+		if [ "$part" = "hypervisor" -a -n "$HYPERVISOR_HOST_TRUNK_INTERFACE" ]
+		then
+			parts="$parts hypervisor-vlans"
+			no_stats_parts="$no_stats_parts hypervisor-vlans"
+		fi
+		if [ "$part" != "stats" ]
+		then
+			no_stats_parts="$no_stats_parts $part"
+		fi
+	done
+	if [ -n "$no_stats_parts" -a "$no_stats_parts" != " network" ]
+	then
+		if [ -z "$flavour" ]
+		then
+			local delimiter=""
+		else
+			local delimiter="."
+		fi
+		variants "$flavour${delimiter}no-stats" $no_stats_parts
+	fi
+	variants "$flavour" $parts
+}
 
-if [ -z $1 ]; then
-    echo "Building docker-compose.yml..."
-    docker-compose  -f ymls/isard-db.yml \
-            -f ymls/isard-engine.yml \
-            -f ymls/isard-static.yml \
-            -f ymls/isard-portal.yml \
-            $HYPER_YAMLS \
-            -f ymls/isard-websockify.yml \
-            -f ymls/isard-squid.yml \
-            -f ymls/isard-webapp.yml \
-            -f ymls/isard-grafana.yml \
-            -f ymls/isard-api.yml \
-            -f ymls/isard-backend.yml \
-            config > docker-compose.no-stats.yml
-        docker-compose \
-                -f docker-compose.no-stats.yml \
-                -f ymls/devel/isard-db.yml.devel \
-                -f ymls/devel/isard-portal.yml.devel \
-                -f ymls/devel/isard-engine.yml.devel \
-                -f ymls/devel/isard-webapp.yml.devel \
-                -f ymls/devel/isard-api.yml.devel \
-                config > docker-compose.no-stats.devel.yml
-    docker-compose \
-            -f docker-compose.no-stats.yml \
-            -f docker-compose.stats.yml \
-            config > docker-compose.yml
-    docker-compose \
-            -f docker-compose.no-stats.devel.yml \
-            -f docker-compose.stats.devel.yml \
-            config > docker-compose.devel.yml
-    echo "You can download the prebuild images and bring it up:"
-    echo "   docker-compose pull && docker-compose up -d"
-    echo "Or build it yourself:"
-    echo "   docker-compose build && docker-compose up -d"
-fi
+## Usage of flavour function
+#
+# flavour <flavour-name> <part-1> <part-2> ...
+# - <flavour-name> is used for the filename: docker-compose.<flavour-name>.yml
+# - <part-1> and <part-2> ... shoud be files like docker-compose-parts/<part-1>.yml
+# - variants build and devel sould be like docker-compose-parts/<part-1>.build.yml
+# and docker-compose-parts/<part-1>.devel.yml
+#
+flavour "" \
+	network \
+	db \
+	engine \
+	static \
+	portal \
+	hypervisor \
+	websockify \
+	squid \
+	webapp \
+	grafana \
+	stats \
+	api \
+	backend \
 
-if [[ $1 == "hypervisor" ]]; then
-    echo "Building hypervisor.yml..."
-    docker-compose  -f ymls/isard-video.yml \
-            $HYPER_YAMLS \
-            -f ymls/isard-websockify.yml \
-            -f ymls/isard-squid.yml \
-            config > docker-compose.hypervisor.no-stats.yml
-    docker-compose \
-            -f docker-compose.hypervisor.no-stats.yml \
-            -f docker-compose.stats.yml \
-            config > docker-compose.hypervisor.yml
-    docker-compose \
-            -f docker-compose.hypervisor.no-stats.yml \
-            -f docker-compose.stats.devel.yml \
-            config > docker-compose.hypervisor.devel.yml
-fi
+flavour hypervisor \
+	network \
+	video \
+	hypervisor \
+	websockify \
+	squid \
+	stats \
 
-if [[ $1 == "hypervisor-standalone" ]]; then
-    echo "Building docker-compose.hypervisor-standalone.yml..."
-    docker-compose $HYPER_YAMLS \
-            -f ymls/isard-hypervisor-standalone.yml \
-            config > docker-compose.hypervisor-standalone.no-stats.yml
-    docker-compose \
-            -f docker-compose.hypervisor-standalone.no-stats.yml \
-            -f docker-compose.stats.yml \
-            config > docker-compose.hypervisor-standalone.yml
-    docker-compose \
-            -f docker-compose.hypervisor-standalone.no-stats.yml \
-            -f docker-compose.stats.devel.yml \
-            config > docker-compose.hypervisor-standalone.devel.yml
-fi
+flavour hypervisor-standalone \
+	network \
+	hypervisor \
+	hypervisor-standalone \
+	stats \
 
-if [[ $1 == "video-standalone" ]]; then
-    echo "Building video-standalone.yml..."
-    docker-compose  -f ymls/isard-video.yml \
-            -f ymls/isard-websockify.yml \
-            -f ymls/isard-squid.yml \
-            config > docker-compose.video-standalone.no-stats.yml
-    docker-compose \
-            -f docker-compose.video-standalone.no-stats.yml \
-            -f docker-compose.stats.yml \
-            config > docker-compose.video-standalone.yml
-    docker-compose \
-            -f docker-compose.video-standalone.no-stats.yml \
-            -f docker-compose.stats.devel.yml \
-            config > docker-compose.video-standalone.devel.yml
-fi
+flavour video-standalone \
+	network \
+	video \
+	websockify \
+	squid \
+	stats \
 
-if [[ $1 == "web" ]]; then
-    echo "Building web.yml..."
-    docker-compose  -f ymls/isard-db.yml \
-            -f ymls/isard-engine.yml \
-            -f ymls/isard-static.yml \
-            -f ymls/isard-portal.yml \
-            -f ymls/isard-webapp.yml \
-            -f ymls/isard-grafana.yml \
-            -f ymls/isard-api.yml \
-            -f ymls/isard-backend.yml \
-            config > docker-compose.web.no-stats.yml
-        docker-compose \
-                -f docker-compose.web.no-stats.yml \
-                -f ymls/devel/isard-db.yml.devel \
-                -f ymls/devel/isardvdi-portal.yml.devel \
-                -f ymls/devel/isard-engine.yml.devel \
-                -f ymls/devel/isard-webapp.yml.devel \
-                config > docker-compose.web.no-stats.devel.yml
-    docker-compose \
-            -f docker-compose.web.no-stats.yml \
-            -f docker-compose.stats.yml \
-            config > docker-compose.web.yml
-    docker-compose \
-            -f docker-compose.web.no-stats.devel.yml \
-            -f docker-compose.stats.devel.yml \
-            config > docker-compose.web.devel.yml
-fi
+flavour web \
+	network \
+	db \
+	engine \
+	static \
+	portal \
+	webapp \
+	grafana \
+	stats \
+	api \
+	backend \
 
+flavour stats \
+	network \
+	stats \
+
+## BUILD_ROOT_PATH sed section
 # Fix the context parameter in the docker-compose file
+# See also BUILD_ROOT_PATH env section above
 sed -i "s|$(pwd)|.|g" docker-compose*.yml
+
 echo "You have the docker-compose files. Have fun!"
+echo "You can download the prebuild images and bring it up:"
+echo "   docker-compose pull && docker-compose up -d"
+echo "Or build it yourself:"
+echo "   docker-compose build && docker-compose up -d"
