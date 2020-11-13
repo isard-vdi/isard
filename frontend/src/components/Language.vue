20001d35d3e3381aac7f6d6097e6be1c911710f2
Simó Albert i Beltran <sim6@bona.gent>
2020-11-13 16:39:52 +0100

    Sort languages

--- frontend/src/components/Language.vue
+++ frontend/src/components/Language.vue
@@ -13,9 +13,9 @@ export default {
   data () {
     return {
       langs: {
+        es: 'Castellano',
         ca: 'Català',
         en: 'English',
-        es: 'Castellano',
         eu: 'Euskara'
       }
     }
