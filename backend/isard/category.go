03f5e9da1eb0ae6623f309f00b85efa885688f75
Néfix Estrada <nefixestrada@gmail.com>
2020-11-27 10:50:32 +0100

    Added the categories endpoint in the backend
    Signed-off-by: Néfix Estrada <nefixestrada@gmail.com>

--- backend/isard/category.go
+++ backend/isard/category.go
@@ -8,6 +8,33 @@ import (
 	"github.com/isard-vdi/isard/backend/pkg/utils"
 )
 
+func (i *Isard) CategoryList(w http.ResponseWriter, r *http.Request) {
+	rsp, err := http.Get(i.url("categories"))
+	if err != nil {
+		i.sugar.Errorw("list categories",
+			"err", err,
+		)
+	}
+
+	defer rsp.Body.Close()
+	if rsp.StatusCode != http.StatusOK {
+		b, _ := ioutil.ReadAll(rsp.Body)
+
+		i.sugar.Errorw("list categories",
+			"err", err,
+			"code", rsp.StatusCode,
+			"body", string(b),
+		)
+
+		err = utils.NewHTTPCodeErr(rsp.StatusCode)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	io.Copy(w, rsp.Body)
+}
+
 func (i *Isard) CategoryLoad(category string, w http.ResponseWriter, r *http.Request) {
 	rsp, err := http.Get(i.url("category/" + category))
 	if err != nil {
