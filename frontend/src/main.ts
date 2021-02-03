d54b5389c0236be634714f554ad2b8b84f6df2ec
Melina Gamboa <melina.devel@gmail.com>
2021-02-03 18:16:59 +0100

    Primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,22 @@
-import { createApp } from "vue";
+// Theme
+import "primevue/resources/themes/saga-blue/theme.css";
+// Core css
+import "primevue/resources/primevue.min.css";
+// Icons
+import "primeicons/primeicons.css";
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
+import "primeflex/primeflex.css";
+
 import App from "./App.vue";
+import PrimeIcons from "primevue/config";
+import PrimeVue from "primevue/config";
+import { createApp } from "vue";
 import router from "./router";
 import store from "./store";
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(PrimeVue)
+  .use(PrimeIcons)
   .mount("#app");
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
+
 import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
