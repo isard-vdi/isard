8f456461bf3a923cfc7f95f3ac6c8d8e12631448
Simó Albert i Beltran <sim6@bona.gent>
2021-02-04 14:13:40 +0100

    Fix derivates value on template list update

--- webapp/webapp/webapp/lib/isardSocketio.py
+++ webapp/webapp/webapp/lib/isardSocketio.py
@@ -59,7 +59,13 @@ class DomainsThread(threading.Thread):
                                 event='template_data'
                                 try:
                                     # New threaded events in ds.py toggles status before it can be processed here.
-                                    data['derivates']=app.adminapi.get_admin_domains_with_derivates(id=c['new_val']['id'],kind='template')
+                                    data[
+                                        "derivates"
+                                    ] = app.adminapi.get_admin_domains_with_derivates(
+                                        id=c["new_val"]["id"], kind="template"
+                                    ).get(
+                                        "derivates"
+                                    )
                                 except:
                                     continue
                                     
