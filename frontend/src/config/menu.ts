a5a6645db09c0c23db50b7cfbf66fabccf483d61
Vittorio Togliatti <info@vittoriotogliatti.com>
2021-02-24 22:02:49 +0100

    TG-18 menu proposal

--- frontend/src/config/menu.ts
+++ frontend/src/config/menu.ts
@@ -1,13 +1,8 @@
 export const menu = [
   {
-    label: 'Login',
-    icon: 'pi pi-fw pi-home',
-    to: { name: 'login' }
-  },
-  {
     label: 'Desktops',
     icon: 'pi pi-fw pi-desktop',
-    to: { name: 'About' }
+    to: { name: 'templates' }
   },
   {
     label: 'Templates',
@@ -15,32 +10,70 @@ export const menu = [
     to: { name: 'templates' }
   },
   {
-    label: 'Configuration',
-    icon: 'pi pi-fw pi-cog',
-    to: { name: 'config' }
+    label: 'Media',
+    icon: 'pi pi-fw pi-circle-off',
+    to: { name: 'templates' }
   },
   {
-    label: 'Submenu prova',
+    label: 'Users',
+    icon: 'pi pi-fw pi-users',
+    to: { name: 'users' }
+  },
+  {
+    label: 'Entities',
     icon: 'pi pi-fw pi-sitemap',
+    to: { name: '' }
+  },
+  {
+    label: 'Groups',
+    icon: 'pi pi-fw pi-list',
+    to: { name: '' }
+  },
+  {
+    label: 'Resources',
+    icon: 'pi pi-fw pi-folder-open',
     items: [
-      { label: 'Login', icon: 'pi pi-fw pi-id-card', to: { name: 'login' } },
-      { label: 'Users', icon: 'pi pi-fw pi-users', to: { name: 'templates' } },
-      {
-        label: 'Entities',
-        icon: 'pi pi-fw pi-users',
-        items: [
-          {
-            label: 'Sub 2',
-            icon: 'pi pi-fw pi-id-card',
-            to: { name: 'login' }
-          },
-          {
-            label: 'Sub 3',
-            icon: 'pi pi-fw pi-users',
-            to: { name: 'templates' }
-          }
-        ]
-      }
+      { label: 'Graphics', to: { name: '' } },
+      { label: 'Video', to: { name: '' } },
+      { label: 'Boots', to: { name: '' } },
+      { label: 'Interfaces', to: { name: '' } },
+      { label: 'Network Qos', to: { name: '' } },
+      { label: 'Disk Qos', to: { name: '' } }
     ]
+  },
+  {
+    label: 'Updates',
+    icon: 'pi pi-fw pi-download',
+    items: [
+      { label: 'Domains', to: { name: '' } },
+      { label: 'Media', to: { name: '' } },
+      { label: 'Virt Installs', to: { name: '' } },
+      { label: 'Virt Builders', to: { name: '' } },
+      { label: 'Video Resources', to: { name: '' } },
+      { label: 'Viewers', to: { name: '' } }
+    ]
+  },
+  {
+    label: 'Graphs',
+    icon: 'pi pi-fw pi-chart-bar',
+    to: { name: '' }
+  },
+  {
+    label: 'Configuration',
+    icon: 'pi pi-fw pi-cog',
+    items: [
+      { label: 'General', to: { name: '' } },
+      { label: 'Roles', to: { name: '' } },
+      { label: 'Job Schedulers', to: { name: '' } },
+      { label: 'Database Backups', to: { name: '' } },
+      { label: 'Authentications', to: { name: '' } },
+      { label: 'Hypervisors', to: { name: '' } },
+      { label: 'Hypervisors pools', to: { name: '' } }
+    ]
+  },
+  {
+    label: 'About',
+    icon: 'pi pi-fw pi-info-circle',
+    to: { name: '' }
   }
 ];
