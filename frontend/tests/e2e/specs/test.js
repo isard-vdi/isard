65ac7f00efc831493d15bb5c217d1afa67b4d396
Vittorio Togliatti <info@vittoriotogliatti.com>
2021-03-02 10:37:42 +0100

    TG-7 e2e test - Login

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -1,8 +1,20 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('Should login', () => {
+  it('Loads the app on pc screen 1920 * 1080 ', () => {
+    cy.viewport(1920, 1080);
     cy.visit('/');
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App');
+    cy.get('.p-card-content').should('be.visible');
+  });
+
+  it('Fill in login credentials', () => {
+    cy.get('#user').type('nefix').should('have.value', 'nefix');
+    cy.get('#password').type('password').should('have.value', 'password');
+  });
+
+  it('Logs in and goes to users page', () => {
+    cy.get('.p-button').click();
+
+    cy.url().should('include', '/users');
   });
 });
