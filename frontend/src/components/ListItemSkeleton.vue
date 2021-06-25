34ea0ad0e352483f851bc81933715683729d2a5a
Vittorio Togliatti <info@vittoriotogliatti.com>
2021-06-25 18:34:44 +0200

    List item skeleton component

---
+++ frontend/src/components/ListItemSkeleton.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class='border-0 mx-3 pb-2'>
+      <b-skeleton width="85%"></b-skeleton>
+      <b-skeleton width="55%"></b-skeleton>
+      <b-skeleton width="70%"></b-skeleton>
+      <br/>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
