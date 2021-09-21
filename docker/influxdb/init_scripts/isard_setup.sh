14cbb10c4575197e065865177e66d943fd0f4e3d
Néfix Estrada <nefixestrada@gmail.com>
2021-09-21 09:47:15 +0200

    Create isardvdi-go bucket

--- docker/influxdb/init_scripts/isard_setup.sh
+++ docker/influxdb/init_scripts/isard_setup.sh
@@ -2,4 +2,8 @@
 
 bucket_id=$(influx bucket list -n isardvdi |grep isardvdi | cut -f1)
 influx auth create --write-bucket $bucket_id -o isardvdi -d hypervisor_remote_bucket_token -u admin -t $INFLUXDB_ADMIN_TOKEN_SECRET
-#influx auth create --write-bucket $bucket_id -o isardvdi -d hypervisor_remote_bucket_token -u isardvdi -t $INFLUXDB_ADMIN_TOKEN_SECRET
+
+influx bucket create -o isardvdi -n isardvdi-go
+bucket_id=$(influx bucket list -n isardvdi-go |grep isardvdi | cut -f1)
+influx auth create --write-bucket $bucket_id -o isardvdi -d hypervisor_remote_bucket_token -u admin -t $INFLUXDB_ADMIN_TOKEN_SECRET
+
