ab5882ab6f05c75a5d2c96253c2289e44eea6f4b
Néfix Estrada <nefixestrada@gmail.com>
2021-11-18 11:40:13 +0100

    fix(authentication): fix authorization header bug (#268)

--- authentication/authentication/authentication.go
+++ authentication/authentication/authentication.go
@@ -77,13 +77,21 @@ func (a *Authentication) Provider(p string) provider.Provider {
 	return prv
 }
 
-type Claims struct {
+type tokenType string
+
+const (
+	tokenTypeUnknown  tokenType = "unknown"
+	tokenTypeLogin    tokenType = "login"
+	tokenTypeRegister tokenType = "register"
+)
+
+type LoginClaims struct {
 	*jwt.StandardClaims
-	KeyID string     `json:"kid"`
-	Data  ClaimsData `json:"data"`
+	KeyID string          `json:"kid"`
+	Data  LoginClaimsData `json:"data"`
 }
 
-type ClaimsData struct {
+type LoginClaimsData struct {
 	Provider   string `json:"provider"`
 	ID         string `json:"user_id"`
 	RoleID     string `json:"role_id"`
@@ -92,15 +100,28 @@ type ClaimsData struct {
 	Name       string `json:"name"`
 }
 
+type RegisterClaims struct {
+	*jwt.StandardClaims
+	KeyID      string `json:"kid"`
+	Type       string `json:"type"`
+	Provider   string `json:"provider"`
+	UserID     string `json:"user_id"`
+	Username   string `json:"username"`
+	CategoryID string `json:"category_id"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	Photo      string `json:"photo"`
+}
+
 func (a *Authentication) signToken(u *model.User) (string, error) {
-	tkn := jwt.NewWithClaims(jwt.SigningMethodHS256, &Claims{
+	tkn := jwt.NewWithClaims(jwt.SigningMethodHS256, &LoginClaims{
 		&jwt.StandardClaims{
 			Issuer:    "isard-authentication",
 			ExpiresAt: time.Now().Add(4 * time.Hour).Unix(),
 		},
 		// TODO: Other signing keys
 		"isardvdi",
-		ClaimsData{
+		LoginClaimsData{
 			u.Provider,
 			u.ID(),
 			u.Role,
@@ -137,20 +158,35 @@ func (a *Authentication) parseToken(ss string, claims jwt.Claims) (*jwt.Token, e
 	return tkn, nil
 }
 
-type RegisterClaims struct {
-	*jwt.StandardClaims
-	KeyID      string `json:"kid"`
-	Type       string `json:"type"`
-	Provider   string `json:"provider"`
-	UserID     string `json:"user_id"`
-	Username   string `json:"username"`
-	CategoryID string `json:"category_id"`
-	Name       string `json:"name"`
-	Email      string `json:"email"`
-	Photo      string `json:"photo"`
-}
+func (a *Authentication) getTokenType(ss string) (*jwt.Token, tokenType, error) {
+	// Register token
+	tkn, err := a.parseToken(ss, &RegisterClaims{})
+	if err == nil {
+		claims, ok := tkn.Claims.(*RegisterClaims)
+		if ok && claims.Type == string(tokenTypeRegister) {
+			return tkn, tokenTypeRegister, nil
+		}
 
-const claimsRegisterType = "register"
+	} else {
+		var e *jwt.ValidationError
+		if !errors.As(err, &e) || e.Errors != jwt.ValidationErrorClaimsInvalid {
+			return nil, tokenTypeUnknown, err
+		}
+	}
+
+	// Login token
+	tkn, err = a.parseToken(ss, &LoginClaims{})
+	if err == nil {
+		return tkn, tokenTypeLogin, nil
+	} else {
+		var e *jwt.ValidationError
+		if !errors.As(err, &e) || e.Errors != jwt.ValidationErrorClaimsInvalid {
+			return nil, tokenTypeUnknown, err
+		}
+	}
+
+	return nil, tokenTypeUnknown, errors.New("unknown token type")
+}
 
 func (a *Authentication) signRegister(u *model.User) (string, error) {
 	tkn := jwt.NewWithClaims(jwt.SigningMethodHS256, &RegisterClaims{
@@ -160,7 +196,7 @@ func (a *Authentication) signRegister(u *model.User) (string, error) {
 		},
 		// TODO: Other signing keys
 		"isardvdi",
-		claimsRegisterType,
+		string(tokenTypeRegister),
 		u.Provider,
 		u.UID,
 		u.Username,
@@ -183,58 +219,64 @@ func (a *Authentication) Login(ctx context.Context, prv, categoryID string, args
 	var redirect string
 	var err error
 
-	// Handle the register flow
+	// Check if the user sends a token
 	if args[provider.TokenArgsKey] != "" {
-		tkn, err := a.parseToken(args[provider.TokenArgsKey], &RegisterClaims{})
-		if err != nil {
-			return "", "", fmt.Errorf("parse register token: %w", err)
-		}
+		tkn, tknType, err := a.getTokenType(args[provider.TokenArgsKey])
+		if err == nil {
+			switch tknType {
+			case tokenTypeRegister:
+				register, ok := tkn.Claims.(*RegisterClaims)
+				if !ok {
+					return "", "", errors.New("invalid register token")
+				}
 
-		register, ok := tkn.Claims.(*RegisterClaims)
-		if !ok {
-			return "", "", errors.New("invalid register token")
-		}
+				u = &model.User{
+					Provider: register.Provider,
+					Category: register.CategoryID,
+					UID:      register.UserID,
+					Username: register.Username,
+				}
+				if err := u.Load(ctx, a.DB); err != nil {
+					if errors.Is(err, model.ErrNotFound) {
+						return "", "", errors.New("user not registered")
+					}
 
-		u = &model.User{
-			Provider: register.Provider,
-			Category: register.CategoryID,
-			UID:      register.UserID,
-			Username: register.Username,
-		}
-		if err := u.Load(ctx, a.DB); err != nil {
-			if errors.Is(err, model.ErrNotFound) {
-				return "", "", errors.New("user not registered")
+					return "", "", fmt.Errorf("load user from db: %w", err)
+				}
+
+				ss, err := a.signToken(u)
+				if err != nil {
+					return "", "", err
+				}
+
+				return ss, redirect, nil
 			}
-
-			return "", "", fmt.Errorf("load user from db: %w", err)
 		}
+	}
 
-		// Normal login flow
-	} else {
-		p := a.Provider(prv)
-		u, redirect, err = p.Login(ctx, categoryID, args)
-		if err != nil {
-			return "", "", fmt.Errorf("login: %w", err)
-		}
+	p := a.Provider(prv)
+	u, redirect, err = p.Login(ctx, categoryID, args)
+	if err != nil {
+		return "", "", fmt.Errorf("login: %w", err)
+	}
 
-		if redirect != "" {
-			return "", redirect, nil
-		}
+	if redirect != "" {
+		return "", redirect, nil
+	}
 
-		exists, err := u.Exists(ctx, a.DB)
-		if err != nil {
-			return "", "", fmt.Errorf("check if user exists: %w", err)
-		}
+	exists, err := u.Exists(ctx, a.DB)
+	if err != nil {
+		return "", "", fmt.Errorf("check if user exists: %w", err)
+	}
 
-		if !exists {
-			// If the user has logged in correctly, but doesn't exist in the DB, they have to register first!
-			ss, err := a.signRegister(u)
-			return ss, "", err
-		}
+	if !exists {
+		// If the user has logged in correctly, but doesn't exist in the DB, they have to register first!
+		ss, err := a.signRegister(u)
+		return ss, "", err
+	}
 
-		if err := u.Load(ctx, a.DB); err != nil {
-			return "", "", fmt.Errorf("load user from DB: %w", err)
-		}
+	if err := u.Load(ctx, a.DB); err != nil {
+		return "", "", fmt.Errorf("load user from DB: %w", err)
 	}
 
 	ss, err := a.signToken(u)
@@ -297,12 +339,12 @@ func (a *Authentication) Callback(ctx context.Context, args map[string]string) (
 }
 
 func (a *Authentication) Check(ctx context.Context, ss string) error {
-	tkn, err := a.parseToken(ss, &Claims{})
+	tkn, err := a.parseToken(ss, &LoginClaims{})
 	if err != nil {
 		return err
 	}
 
-	_, ok := tkn.Claims.(*Claims)
+	_, ok := tkn.Claims.(*LoginClaims)
 	if !ok {
 		return errors.New("unknown JWT claims format")
 	}
