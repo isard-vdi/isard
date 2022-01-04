f325e45c5ee0e33ee028f71f823f33a913ce2601
Vittorio Togliatti <info@vittoriotogliatti.com>
2022-01-04 12:38:42 +0100

    feat(frontend): add checkbox to show only started desktops in deployment view

--- frontend/src/components/DeploymentBar.vue
+++ frontend/src/components/DeploymentBar.vue
@@ -5,9 +5,28 @@
         <div class="separator"></div>
         <div class="d-flex flex-grow">
 
+          <!-- Left aligned nav items-->
+          <b-navbar-nav id="statusbar-content" class="flex-grow flex-row">
+            <!-- Only started checkbox -->
+            <b-nav-item class="ml-2 ml-md-4" href="#" @click="toggleStartedFilter">
+              <div>
+                <b-form-checkbox
+                  id="started-checkbox"
+                  name="checkbox-1"
+                  v-model="started"
+                  value=true
+                  unchecked-value=false
+                  aria-hidden="true"
+                  class="mr-2 mr-lg-0">
+                    <p class="d-none d-md-inline p-0 m-0">{{$t("components.statusbar.checkbox-text")}}</p>
+                    <p class="d-inline d-md-none  p-0 m-0">{{$t("components.statusbar.checkbox-text-short")}}</p>
+                </b-form-checkbox>
+              </div>
+            </b-nav-item>
+          </b-navbar-nav>
+
           <!-- Right aligned nav items-->
           <b-navbar-nav class="ml-auto flex-row d-none d-xl-flex">
-
             <b-nav-item href="#" @click="changeView('grid')" :class="{selectedView: getViewType === 'grid'}">
               <b-icon
                 icon="grid-fill"
@@ -15,7 +34,6 @@
                 class="text-medium-gray mt-1"
               ></b-icon>
             </b-nav-item>
-
             <b-nav-item href="#" @click="changeView('youtube')" :class="{selectedView: getViewType === 'youtube'}" class="ml-sm-2 ml-xl-0">
               <b-icon
                 icon="grid1x2-fill"
@@ -23,7 +41,6 @@
                 class="text-medium-gray mt-1"
               ></b-icon>
             </b-nav-item>
-
           </b-navbar-nav>
         </div>
       </b-navbar>
@@ -32,12 +49,24 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
+  data () {
+    return {
+      started: false
+    }
+  },
   methods: {
+    ...mapActions([
+      'toggleDeploymentsShowStarted'
+    ]),
     changeView (type) {
       this.$store.dispatch('setViewType', type)
+    },
+    toggleStartedFilter () {
+      this.started = !this.started
+      this.toggleDeploymentsShowStarted()
     }
   },
   computed: {
