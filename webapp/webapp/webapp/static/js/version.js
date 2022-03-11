470a3c1b0c4cc466d23c5f0d03da13dff95d4361
montlili03 <lidiamontero165@gmail.com>
2022-03-11 09:37:29 +0100

    refactor(webapp): isardvdi current version working with apiv3

--- webapp/webapp/webapp/static/js/version.js
+++ webapp/webapp/webapp/static/js/version.js
@@ -1,8 +1,10 @@
 $.ajax({
     type: "GET",
-    url:"/api/v3/",
+    url:"/api/v3",
     success: function (data) {
         var isardvdi_version = data.isardvdi_version.split(" ");
-        $("#versplit").text(isardvdi_version[0]);
+        var versplit = isardvdi_version[0];
+        var releaseUrl = "http://gitlab.com/isard/isardvdi/-/releases/v" + data.isardvdi_version.split(" ")[0]
+        $("#version").text(data.isardvdi_version).prop("href", releaseUrl)
     }
 })
