cf800ef2a8d127617521fb3b13e390637ad8db12
darta <josepmaria@isardvdi.com>
2022-03-18 14:14:41 +0100

    fix(webapp): added unflatten generic function

--- webapp/webapp/webapp/static/isard.js
+++ webapp/webapp/webapp/static/isard.js
@@ -205,6 +205,24 @@ $(document).ready(function() {
     })
 });
 
+JSON.unflatten = function(data) {
+    "use strict";
+    if (Object(data) !== data || Array.isArray(data))
+        return data;
+    var regex = /\-?([^-\[\]]+)|\[(\d+)\]/g,
+        resultholder = {};
+    for (var p in data) {
+        var cur = resultholder,
+            prop = "",
+            m;
+        while (m = regex.exec(p)) {
+            cur = cur[prop] || (cur[prop] = (m[2] ? [] : {}));
+            prop = m[2] || m[1];
+        }
+        cur[prop] = data[p];
+    }
+    return resultholder[""] || resultholder;
+};
 
 // Form serialization
 
