628e4bcfcddf184a0c26d3b54ed7775d88f8726f
montlili03 <lidia@isardvdi.com>
2022-05-24 08:25:17 +0200

    refactor(api,webapp): desktop server button works in user desktops list

--- api/src/api/views/AdminDomainsView.py
+++ api/src/api/views/AdminDomainsView.py
@@ -13,7 +13,9 @@ from flask import request
 from api import app
 
 from ..libv2.api_admin import ApiAdmin, admin_table_get, admin_table_update
-from .decorators import is_admin_or_manager
+from ..libv2.api_exceptions import Error
+from ..libv2.validators import _validate_item
+from .decorators import has_token, is_admin_or_manager, ownsDomainId
 
 admins = ApiAdmin()
 
@@ -45,28 +47,6 @@ def api_v3_admin_domains_xml(payload, id):
         admin_table_update("domains", data)
     return (
         json.dumps(admin_table_get("domains", pluck="xml", id=id)["xml"]),
-        200,
-        {"Content-Type": "application/json"},
-    )
-
-
-@app.route("/api/v3/admin/domains/server/<id>", methods=["POST", "GET"])
-@is_admin_or_manager
-def api_v3_admin_domains_server(payload, id):
-    if request.method == "POST":
-        admin_table_update("domains", id, request.get_json(force=True))
-        return (
-            json.dumps(admin_table_update),
-            200,
-            {"Content-Type": "application/json"},
-        )
-    server = admin_table_get(
-        "domains",
-        pluck="create_dict",
-        id=id,
-    )["create_dict"]
-    return (
-        json.dumps(server.get("server", False)),
         200,
         {"Content-Type": "application/json"},
     )
