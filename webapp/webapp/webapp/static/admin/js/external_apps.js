115084fcda910f3bcf2b95a770202e72b789e12e
darta <josepmaria@isardvdi.com>
2022-05-29 23:01:50 +0200

    fix(webapp): removed unused external_apps code

--- webapp/webapp/webapp/static/admin/js/external_apps.js
+++ webapp/webapp/webapp/static/admin/js/external_apps.js
@@ -5,7 +5,6 @@
 * License: AGPLv3
 */
 
-//~ tables=['roles','categories','groups']
 $(document).ready(function() {
     var table=$('#externalapps').DataTable( {
         "ajax": {
@@ -26,43 +25,5 @@ $(document).ready(function() {
             { "data": "domain"},
         ]
     } );
-
-    
-	$('.btn-new-role').on('click', function () {
-        setQuotaMax('#roles-quota');
-			$('#modalAddRole').modal({
-				backdrop: 'static',
-				keyboard: false
-			}).modal('show');
-            $('#modalAddRoleForm')[0].reset();
-            //~ setModalAddUser();
-	});    
-
-    $("#modalAddRole #send").on('click', function(e){
-            var form = $('#modalAddRoleForm');
-            form.parsley().validate();
-            if (form.parsley().isValid()){
-                data=$('#modalAddRoleForm').serializeObject();
-                data['table']='roles';
-                socket.emit('role_category_group_add',data)  
-            }
-        }); 
-            
 });
 
-function formatRoles ( d ) {
-    // `d` is the original data object for the row
-    var cells=''
-            //~ '<div class="btn-group"> \
-                    //~ <button class="btn btn-sm btn-default btn-edit" id="btn-edit" type="button"  data-placement="top" data-toggle="tooltip" data-original-title="Edit"><i class="fa fa-pencil"></i></button> \
-               //~ </div>';
-    for(var k in d){
-		cells+='<tr>'+
-					'<td>'+k+':</td>'+
-					'<td>'+d[k]+'</td>'+
-				'</tr>'
-	}
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-        cells+
-    '</table>';
-}
