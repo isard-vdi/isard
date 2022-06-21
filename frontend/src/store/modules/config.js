fb1f1368352c03bd67bea80a3b5d8a00a9ba4386
Miriam Melina Gamboa Valdez <melina@isardvdi.com>
2022-06-21 16:00:09 +0200

    refactor(frontend): config call to api instead of authentication

--- frontend/src/store/modules/config.js
+++ frontend/src/store/modules/config.js
@@ -1,25 +1,34 @@
 import axios from 'axios'
-import { authenticationSegment } from '../../shared/constants'
+import { apiV3Segment, authenticationSegment } from '../../shared/constants'
 
 export default {
   state: {
-    config: {
-      providers: []
-    }
+    providers: [],
+    config: {}
   },
   getters: {
+    getProviders: state => {
+      return state.providers
+    },
     getConfig: state => {
       return state.config
     }
   },
   mutations: {
+    setProviders: (state, providers) => {
+      state.providers = providers
+    },
     setConfig: (state, config) => {
       state.config = config
     }
   },
   actions: {
+    async fetchProviders ({ commit }) {
+      const rsp = await axios.get(`${authenticationSegment}/providers`)
+      commit('setProviders', rsp.data.providers)
+    },
     async fetchConfig ({ commit }) {
-      const rsp = await axios.get(`${authenticationSegment}/config`)
+      const rsp = await axios.get(`${apiV3Segment}/user/config`)
       commit('setConfig', rsp.data)
     }
   }
