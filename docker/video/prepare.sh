d3c10d94f368d551f7922a656f0864ba232d865d
darta <josepmaria@isardvdi.com>
2022-09-22 08:41:21 +0200

    fix(portal,video): allow custom video and portal certs in separate file

--- docker/video/prepare.sh
+++ docker/video/prepare.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+if [ -f /certs/custom-video-chain.pem ]
+then
+  sed -i 's/\/chain.pem/\/custom-video-chain.pem/g' /usr/local/etc/haproxy/haproxy.cfg
+else
+  sed -i 's/\/custom-video-chain.pem/\/chain.pem/g' /usr/local/etc/haproxy/haproxy.cfg
+fi
 LETSENCRYPT_DOMAIN="$VIDEO_DOMAIN" LETSENCRYPT_EMAIL="$LETSENCRYPT_EMAIL" letsencrypt.sh
