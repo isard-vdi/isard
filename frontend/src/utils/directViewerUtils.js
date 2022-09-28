0fca23a0030eb399969f1ba9915cf90845f006d9
Naomi Hidalgo <naomi@isardvdi.com>
2022-09-28 13:43:45 +0200

    fix(frontend): direct viewer invalid date fixed

--- frontend/src/utils/directViewerUtils.js
+++ frontend/src/utils/directViewerUtils.js
@@ -9,7 +9,7 @@ export class DirectViewerUtils {
       description,
       viewers,
       state,
-      shutdown: i18n.t('message-modal.messages.desktop-time-limit', { name: name, date: DateUtils.formatAsTime(DateUtils.utcToLocalTime(scheduled.shutdown)) }),
+      shutdown: scheduled.shutdown ? i18n.t('message-modal.messages.desktop-time-limit', { name: name, date: DateUtils.formatAsTime(DateUtils.utcToLocalTime(scheduled.shutdown)) }) : false,
       jwt,
       desktopId
     }
