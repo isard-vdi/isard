44ae91a7e020b77ab2ce89c31001e0eb292a644b
Naomi Hidalgo <naomi@isardvdi.com>
2022-09-30 10:22:43 +0200

    fix(engine): status_logs field and fixed some missing db connection closes

--- engine/engine/engine/services/db/domains.py
+++ engine/engine/engine/services/db/domains.py
@@ -4,7 +4,6 @@ import sys
 import time
 import traceback
 from copy import deepcopy
-from datetime import datetime
 
 import flatten_dict
 from engine.config import TRANSITIONAL_STATUS
@@ -176,11 +175,10 @@ def update_domain_status(
         hyp_id = rtable.get(id_domain).pluck("hyp_started").run(r_conn)["hyp_started"]
 
     if hyp_id is None:
-        # print('ojojojo')rtable.get(id_domain)
         results = (
             rtable.get(id_domain)
             .update(
-                {"status": status, "hyp_started": "", "detail": json.dumps(detail)},
+                {"status": status, "hyp_started": False, "detail": json.dumps(detail)},
                 return_changes=True,
             )
             .run(r_conn)
@@ -192,7 +190,7 @@ def update_domain_status(
             "detail": json.dumps(detail),
         }
         if update_started_time is True:
-            d_update["started_time"] = int(datetime.now().timestamp())
+            d_update["status_logs"] = {"Started": time.time()}
 
         results = (
             rtable.get(id_domain).update(d_update, return_changes=True).run(r_conn)
@@ -226,6 +224,7 @@ def update_domain_hw_stats(hw_stats, id_domain):
     r_conn = new_rethink_connection()
     rtable = r.table("domains")
     results = rtable.get(id_domain).update({"hw_stats": hw_stats}).run(r_conn)
+    close_rethink_connection(r_conn)
     return results
 
 
@@ -299,7 +298,7 @@ def get_domain_hyp_started(id_domain):
     results = rtable.get(id_domain).pluck("hyp_started").run(r_conn)
     close_rethink_connection(r_conn)
     if results is None:
-        return ""
+        return False
 
     return results["hyp_started"]
 
@@ -351,10 +350,10 @@ def get_domain_hyp_started_and_status_and_detail(id_domain):
         results = (
             rtable.get(id_domain).pluck("hyp_started", "detail", "status").run(r_conn)
         )
-        close_rethink_connection(r_conn)
     except:
         # if results is None:
         return {}
+    close_rethink_connection(r_conn)
     return results
 
 
@@ -838,7 +837,7 @@ def get_vgpu_uuid_status(uuid, gpu_id=None, profile=None):
         logs.exception_id.debug("0080")
         logs.main.error(e)
         return False
-
+    close_rethink_connection(r_conn)
     return False
 
 
@@ -1140,6 +1139,7 @@ def get_domains_flag_server_to_starting():
     except Exception as e:
         logs.exception_id.debug("0040")
         logs.main.error(e)
+        close_rethink_connection(r_conn)
         return []
     close_rethink_connection(r_conn)
     return ids_servers_must_start
@@ -1160,6 +1160,7 @@ def update_domain_history_from_id_domain(domain_id, new_status, new_detail, date
             f"domain {domain_id} does not exists in db and update_domain_history_from_id_domain is not posible"
         )
         logs.main.error(e)
+        close_rethink_connection(r_conn)
         return False
     close_rethink_connection(r_conn)
 
@@ -1174,7 +1175,7 @@ def update_domain_history_from_id_domain(domain_id, new_status, new_detail, date
     if "hyp_started" in domain_fields:
         hyp_started = domain_fields["hyp_started"]
     else:
-        hyp_started = ""
+        hyp_started = False
 
     # now = date_now.strftime("%Y-%b-%d %H:%M:%S.%f")
     now = int(time.time())
@@ -1394,6 +1395,7 @@ def domain_get_vgpu_info(domain_id):
     r_conn = new_rethink_connection()
     rtable_dom = r.table("domains")
     d = dict(rtable_dom.get(domain_id).pluck("vgpu_info").run(r_conn))
+    close_rethink_connection(r_conn)
     return d.get("vgpu_info", {})
 
 
@@ -1430,7 +1432,11 @@ def remove_fieds_when_stopped(id_domain, conn=False):
         r_conn = conn
 
     r.table("domains").get(id_domain).update(
-        {"create_dict": {"personal_vlans": False}, "hyp_started": False}
+        {
+            "create_dict": {"personal_vlans": False},
+            "hyp_started": False,
+            "status_logs": {"Stopped": time.time()},
+        },
     ).run(r_conn)
 
     if conn is False:
