5b618a0a9927734ec04cb871a015d1cd8005e71d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-07 04:49:15 +0000

    Adding tarfile member sanitization to extractall()

--- webapp/webapp/webapp/lib/admin_api.py
+++ webapp/webapp/webapp/lib/admin_api.py
@@ -65,7 +65,26 @@ class isardAdmin:
         handler.save(os.path.join(path + filename))
 
         with tarfile.open(path + handler.filename, "r:gz") as tar:
-            tar.extractall(path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path)
             tar.close()
         with open(path + id + ".rethink", "rb") as isard_rethink_file:
             isard_rethink = pickle.load(isard_rethink_file)
