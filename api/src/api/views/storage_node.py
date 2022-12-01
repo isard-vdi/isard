4a06ad64058d5277ac7ab1745c8edae5a96bc9ad
Simó Albert i Beltran <sim6@bona.gent>
2022-12-01 12:02:40 +0100

    chore(api): manage storage node endpoint
    Signed-off-by: Simó Albert i Beltran <sim6@bona.gent>

---
+++ api/src/api/views/storage_node.py
@@ -0,0 +1,54 @@
+#
+#   IsardVDI - Open Source KVM Virtual Desktops based on KVM Linux and dockers
+#   Copyright (C) 2022 Simó Albert i Beltran
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import json
+
+import requests
+from flask import request
+
+from api import app
+
+from .._common.api_exceptions import Error
+from ..libv2.storage_node import StorageNode
+from .decorators import is_hyper
+
+
+@app.route("/api/v3/storage_node", methods=["POST", "PUT", "DELETE"])
+@is_hyper
+def manage_storage_node():
+    """
+    Manage Storage Node.
+
+    Input data must be JSON.
+    """
+    if not request.is_json:
+        raise Error(description="JSON expected")
+    storage_node = StorageNode(**request.json)
+    if request.method == "DELETE":
+        storage_node.status = "deleted"
+    elif (
+        storage_node.api_base_url
+        and requests.get(storage_node.api_base_url).status_code == 200
+    ):
+        storage_node.status = "online"
+    return (
+        json.dumps(storage_node.id),
+        200,
+        {"Content-Type": "application/json"},
+    )
