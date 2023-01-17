97eb10ae66aef9a3a7b612e4bc9073b198bc48b4
darta <josepmaria@isardvdi.com>
2023-01-17 20:15:35 +0100

    fix(api): get actual gpu planning

--- api/src/api/views/bookings/ReservablesView.py
+++ api/src/api/views/bookings/ReservablesView.py
@@ -79,10 +79,10 @@ def api_v3_reservable_items_enabled(payload, reservable_type, item_id):
 @app.route("/api/v3/admin/reservables_planner/actual_plan/<item_id>", methods=["GET"])
 @is_admin
 def api_v3_reservables_planner_get_item_actual_plan(payload, item_id):
-    plan = api_rp.get_item_actual_plan(item_id)
-    if not plan:
+    plan = api_rp.list_item_plans(item_id)
+    if not len(plan):
         return json.dumps({})
-    return json.dumps(plan)
+    return json.dumps(plan[0])
 
 
 # Gets actual plans for item (card) /subitem (profile) reservable resources
