eb23a203c659884dc497511d08e2fc5aa0220a64
Néfix Estrada <nefixestrada@gmail.com>
2023-02-07 09:48:44 +0100

    feat(monitor): add grafana customizations

--- docker/grafana/run.sh
+++ docker/grafana/run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+if [[ -d "/custom/dashboards" ]]; then
+    cp -RT /custom/dashboards/ /etc/grafana/provisioning/dashboards/
+fi
+
+if [[ -d "/custom/alerting" ]]; then
+    cp -RT /custom/alerting/ /etc/grafana/provisioning/alerting/
+fi
+
 grafana-cli admin reset-admin-password $WEBAPP_ADMIN_PWD
