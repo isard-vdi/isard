39bc92f305e455cd34931ddbf5b7756ebae6a253
Simó Albert i Beltran <sim6@bona.gent>
2023-05-10 14:34:45 +0200

    Revert "chore(api): werkzeug resends SIGTERM to reloader child process"
    This reverts commit 135e0291efc6d3be4a11a5eba0c35af0de942691 due to not needed any more.
    
    Signed-off-by: Simó Albert i Beltran <sim6@bona.gent>

--- api/src/startv3.py
+++ api/src/startv3.py
@@ -17,7 +17,6 @@ from api.libv2 import (
 )
 from api.libv2.bookings import api_socketio_bookings, api_socketio_plannings
 from api.libv2.maintenance import Maintenance
-from werkzeug_reloader_send_signal import reloader_options
 
 from api import app, socketio
 
@@ -42,12 +41,4 @@ if __name__ == "__main__":
     api_admin_socketio.start_resources_thread()
 
     signal.signal(signal.SIGTERM, lambda *args: sys.exit(0))
-    socketio.run(
-        app,
-        host="0.0.0.0",
-        port=5000,
-        debug=debug,
-        log_output=debug,
-        # https://github.com/pallets/werkzeug/pull/2633
-        reloader_options=reloader_options,
-    )
+    socketio.run(app, host="0.0.0.0", port=5000, debug=debug, log_output=debug)
