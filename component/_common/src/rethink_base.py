8539b5de1ad6033b2e1043380327583fca0023f0
Simó Albert i Beltran <sim6@bona.gent>
2023-05-24 16:43:23 +0200

    chore(common): rethinkdb objects equality
    Signed-off-by: Simó Albert i Beltran <sim6@bona.gent>

--- component/_common/src/rethink_base.py
+++ component/_common/src/rethink_base.py
@@ -95,6 +95,14 @@ class RethinkBase(ABC):
             )
         updated_data["id"] = self.id
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.id == other.id
+        return False
+
+    def __hash__(self):
+        return hash(self.id)
+
     @classmethod
     def exists(cls, document_id):
         """
