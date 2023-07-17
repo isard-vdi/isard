e4fc01614d14bd210bd5bc698ef2b352d51e8e11
darta <josepmaria@isardvdi.com>
2023-07-17 22:49:18 +0200

    refactor(webapp): cleaning and headers

--- webapp/webapp/webapp/__init__.py
+++ webapp/webapp/webapp/__init__.py
@@ -1,10 +1,22 @@
-# Copyright 2017 the Isard-vdi project authors:
-#      Josep Maria Viñolas Auquer
-#      Alberto Larraz Dalmases
-# License: AGPLv3
-
-#!flask/bin/python
-# coding=utf-8
+#
+#   Copyright © 2023 Josep Maria Viñolas Auquer, Alberto Larraz Dalmases
+#
+#   This file is part of IsardVDI.
+#
+#   IsardVDI is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or (at your
+#   option) any later version.
+#
+#   IsardVDI is distributed in the hope that it will be useful, but WITHOUT ANY
+#   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+#   details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with IsardVDI. If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 import os
 
--- webapp/webapp/webapp/auth/authentication.py
+++ webapp/webapp/webapp/auth/authentication.py
@@ -1,12 +1,28 @@
-# Copyright 2017 the Isard-vdi project authors:
-#      Josep Maria Viñolas Auquer
-#      Alberto Larraz Dalmases
-# License: AGPLv3
+#
+#   Copyright © 2023 Josep Maria Viñolas Auquer, Alberto Larraz Dalmases
+#
+#   This file is part of IsardVDI.
+#
+#   IsardVDI is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or (at your
+#   option) any later version.
+#
+#   IsardVDI is distributed in the hope that it will be useful, but WITHOUT ANY
+#   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+#   details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with IsardVDI. If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 import json
 import time
 
 import requests
+import rethinkdb as r
 from flask import request
 from flask_login import LoginManager, UserMixin
 from rethinkdb import RethinkDB
@@ -23,7 +39,6 @@ from ..lib.flask_rethink import RDB
 db = RDB(app)
 db.init_app(app)
 
-from ..lib.log import *
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -168,7 +183,6 @@ class auth(object):
     def authentication_local(self, username, password):
         with app.app_context():
             dbuser = r.table("users").get(username).run(db.conn)
-            # log.info('USER:'+username)
             if dbuser is None or dbuser["active"] is not True:
                 return False
             dbuser["group_uid"] = (
