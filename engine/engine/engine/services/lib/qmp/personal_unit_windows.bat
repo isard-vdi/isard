cf4fd94a5a576e638fe259cf0d9c3fa29819ee24
Néfix Estrada <nefixestrada@gmail.com>
2023-08-26 19:12:59 +0200

    fix(engine): wait for network when mounting the personal unit in Windows

--- engine/engine/engine/services/lib/qmp/personal_unit_windows.bat
+++ engine/engine/engine/services/lib/qmp/personal_unit_windows.bat
@@ -1,3 +1,11 @@
 REM We use 'more' to ensure the encoding is ansi and it can be executed
-echo Set oShell = CreateObject("Wscript.Shell") | more > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp/personal_unit.vbs"
-echo Return =  oShell.Run("net use Z: http{protocol}://{host}/ {password} /user:{user} /persistent:no", 0, true) | more >> "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp/personal_unit.vbs"
+set scriptPath="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp/isard_personal_unit.vbs"
+
+REM Remove the file right when the execution starts, so there are less changes of leaked credentials
+echo Set oFS = CreateObject("Scripting.FileSystemObject") | more > %scriptPath%
+echo oFS.DeleteFile(WScript.ScriptFullName) | more >> %scriptPath%
+
+REM Wait for a network connection to the server and make the mount afterwards
+echo Set oShell = CreateObject("Wscript.Shell") | more >> %scriptPath%
+echo Return = oShell.Run("powershell -Command do {{ $ping = test-connection -comp {host} -count 1 -Quiet }} until ($ping)", 0, true) | more >> %scriptPath%
+echo Return = oShell.Run("net use Z: http{protocol}://{host}/ {password} /user:{user} /persistent:no", 0, true) | more >> %scriptPath%
