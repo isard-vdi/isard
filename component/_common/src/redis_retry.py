b68b8b7b1878c9c0915a73ef56052f7ae5704f96
Simó Albert i Beltran <sim6@bona.gent>
2023-10-03 12:35:03 +0200

    chore(_common): add RedisRetry a Redis class that retries execute_command on connection error
    Signed-off-by: Simó Albert i Beltran <sim6@bona.gent>

---
+++ component/_common/src/redis_retry.py
@@ -0,0 +1,38 @@
+#
+#   IsardVDI - Open Source KVM Virtual Desktops based on KVM Linux and dockers
+#   Copyright (C) 2023 Simó Albert i Beltran
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from time import sleep
+
+from redis import Redis
+from redis.exceptions import ConnectionError
+
+RETRY_INTERVAL = 5
+
+
+class RedisRetry(Redis):
+    def execute_command(self, *args, **kwargs):
+        while True:
+            try:
+                return super().execute_command(*args, **kwargs)
+            except ConnectionError as exception:
+                print(
+                    f"Redis Connection Error: {exception} Retrying in {RETRY_INTERVAL} secconds",
+                    flush=True,
+                )
+                sleep(RETRY_INTERVAL)
