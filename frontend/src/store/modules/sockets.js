6342425fb38a7acf48f5b10595342b91af3d7b68
Miriam Melina Gamboa Valdez <melina.devel@gmail.com>
2023-12-11 19:36:56 +0100

    fix(frontend): socket connect events only handled when open channel

--- frontend/src/store/modules/sockets.js
+++ frontend/src/store/modules/sockets.js
@@ -1,39 +1,36 @@
 import { socket } from '@/utils/socket-instance'
 import store from '@/store/index.js'
 
-// socket.onAny((event, ...args) => {
-//   console.log('onAny')
-//   console.log(event, args)
-// })
-socket.on('connect', function () {
-  console.log('WS connected')
-})
-socket.on('connect_error', (err) => {
-  if (err.message === 'timeout') {
-    console.log('WS connection timeout')
-  } else if (err.message === 'websocket error') {
-    console.log('WS connection error')
-  } else if (err.message === 'Connection rejected by server') {
-    console.log('WS connection not authorized')
-    store.dispatch('logout')
-  } else {
-    console.log('WS connection error: ' + err)
-  }
-})
-
 export default {
   actions: {
     openSocket (context, { jwt, room }) {
       if (!socket.connected) {
-        socket.auth.jwt = localStorage.token
+        socket.auth.jwt = localStorage.token ? localStorage.token : jwt
         socket.io.opts.query = {
           room
         }
         socket.open()
+        socket.on('connect', function () {
+          console.log('WS connected')
+        })
+        socket.on('connect_error', (err) => {
+          if (err.message === 'timeout') {
+            console.log('WS connection timeout')
+          } else if (err.message === 'websocket error') {
+            console.log('WS connection error')
+          } else if (err.message === 'Connection rejected by server') {
+            console.log('WS connection not authorized')
+            store.dispatch('logout')
+          } else {
+            console.log('WS connection error: ' + err)
+          }
+        })
       }
     },
     closeSocket (context) {
       if (socket.connected) {
+        socket.off('connect')
+        socket.off('connect_error')
         socket.close()
       }
     }
