50a9c16deeedf179498afcc5d3b20442093f7f58
darta <josepmaria@isardvdi.com>
2023-12-15 11:15:08 +0100

    fix: build from main git repo

--- docker/hypervisor/spice-dev/build-spice-dev.sh
+++ docker/hypervisor/spice-dev/build-spice-dev.sh
@@ -19,11 +19,29 @@ makedepends="
 	"
 apk add $makedepends
 
-source="https://www.spice-space.org/download/releases/spice-server/spice-$pkgver.tar.bz2"
-wget -O- "$source" | tar xj || exit 1
-cd spice-$pkgver
-  ./configure --prefix=/usr --sysconfdir=/etc \
-        --localstatedir=/var --libdir=/usr/lib
-make
-make install
+## FROM GIT
+apk add git
+apk add abuild
+git clone https://gitlab.freedesktop.org/spice/spice.git
+cd spice
+git checkout fe1c25f530b95d32cc81bc1a395d80ace631d2dd
+	abuild-meson \
+		-Db_lto=true \
+		-Dgstreamer=1.0 \
+		-Dlz4=true \
+		-Dsasl=true \
+		-Dopus=enabled \
+		-Dsmartcard=disabled \
+		. output
+	meson compile -C output
+
+## FROM TAR (As in alpine pkg build)
+# source="https://www.spice-space.org/download/releases/spice-server/spice-$pkgver.tar.bz2"
+# wget -O- "$source" | tar xj || exit 1
+# cd spice-$pkgver
+
+# ./configure --prefix=/usr --sysconfdir=/etc \
+# 	--localstatedir=/var --libdir=/usr/lib
+# make
+# make install
 cp /usr/lib/libspice-server.so.1* /isard/volatiles/
