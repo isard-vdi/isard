fa0f4ae519c069577c984b305986e0c1835675f4
darta <josepmaria@isardvdi.com>
2023-12-29 10:47:49 +0100

    fix(engine): removed supervisord

--- engine/docker/genrsa.sh
+++ engine/docker/genrsa.sh
@@ -5,23 +5,4 @@ if [ ! -f /root/.ssh/id_rsa ]
 then
     echo "Generating new rsa keys..."
     cat /dev/zero | ssh-keygen -q -N ""
-
-    echo -e "Host isard-hypervisor\n \
-        StrictHostKeyChecking no" >/root/.ssh/config
-    chmod 600 /root/.ssh/config
-
-    echo "Checking for isard-hypervisor ssh..."
-
-    i=0
-    while ! nc -z isard-hypervisor 22; do   
-      sleep 0.5
-      ((i++))
-      if [[ "$i" == '25' ]]; then
-        break
-      fi  
-      echo "Checking for isard-hypervisor shh"
-    done
-
-    echo "Adding isard-hypervisor keys"
-    ssh-keyscan -T 10 isard-hypervisor > /root/.ssh/known_hosts
 fi
