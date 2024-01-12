854b9530267eaf388ac8ee2d3fb2c5eb0464522a
darta <josepmaria@isardvdi.com>
2024-01-12 13:13:46 +0100

    fix(webapp): show in hypervisors viewers status

--- webapp/webapp/webapp/static/admin/js/hypervisors.js
+++ webapp/webapp/webapp/static/admin/js/hypervisors.js
@@ -272,10 +272,33 @@ $(document).ready(function() {
         "render": renderBoolean
       },
       {
+        // Static
+        "targets": 16,
+        "render": function(data, type, full, meta) {
+          if ("viewer_status" in full){
+            if (full.viewer_status.static) {
+              return '<i class="fa fa-circle" aria-hidden="true" style="color:green"> Expires in '+full.viewer_status.static+' days</i>  '+data
+            } else {
+              return '<i class="fa fa-circle" aria-hidden="true" style="color:red"> ERROR Expires in '+full.viewer_status.static+' days</i>  '+data
+            }
+          } else {
+            return '<i class="fa fa-spinner fa-lg fa-spin"></i>  '+data
+          }
+        }
+      },
+      {
         // Proxy Video
         "targets": 17,
         "render": function(data, type, full, meta) {
-          return full.viewer.proxy_video + ' (' + full.viewer.spice_ext_port + ',' + full.viewer.html5_ext_port + ')';
+          if ("viewer_status" in full){
+            title="HTML5 cert: "+full.viewer_status.html5+" days\nSpice cert: "+full.viewer_status.spice+" days\nStatic cert: "+full.viewer_status.static+" days"
+            if (full.viewer_status.html5 && full.viewer_status.spice && full.viewer_status.static) {
+              return '<i class="fa fa-circle" aria-hidden="true" style="color:green" title="'+title+'"> Expires in '+full.viewer_status.html5+' days</i>  '+full.viewer.proxy_video + ' (' + full.viewer.spice_ext_port + ',' + full.viewer.html5_ext_port + ')'
+            } else {
+              return '<i class="fa fa-circle" aria-hidden="true" style="color:red" title="'+title+'"> ERROR Expires in '+full.viewer_status.html5+' days</i>  '+full.viewer.proxy_video + ' (' + full.viewer.spice_ext_port + ',' + full.viewer.html5_ext_port + ')'
+            }
+          }
+          return '<i class="fa fa-spinner fa-lg fa-spin"></i>  '+full.viewer.proxy_video + ' (' + full.viewer.spice_ext_port + ',' + full.viewer.html5_ext_port + ')'
         }
       },
 
