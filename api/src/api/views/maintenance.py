84409960771b862159e18d7593bf57c075c17251
Naomi Hidalgo <naomi@isardvdi.com>
2024-03-12 16:24:19 +0100

    feat(webapp,api,frontend): custom maintenance text

--- api/src/api/views/maintenance.py
+++ api/src/api/views/maintenance.py
@@ -26,7 +26,8 @@ from flask import request
 from api import app
 
 from ..libv2.maintenance import Maintenance
-from .decorators import is_admin
+from ..libv2.validators import _validate_item
+from .decorators import is_admin, maintenance
 
 
 @cached(cache=TTLCache(maxsize=1, ttl=5))
@@ -45,6 +46,56 @@ def _api_maintenance_put(payload):
     Maintenance.enabled = request.get_json()
     return (
         json.dumps(Maintenance.enabled),
+        200,
+        {"Content-Type": "application/json"},
+    )
+
+
+@app.route("/api/v3/maintenance/text", methods=["PUT"])
+@is_admin
+def _api_maintenance_text_put(payload):
+    data = request.get_json()
+
+    data = _validate_item("maintenance_text", data)
+    return (
+        json.dumps(Maintenance.update_text(data)),
+        200,
+        {"Content-Type": "application/json"},
+    )
+
+
+@app.route("/api/v3/maintenance/text", methods=["GET"])
+@is_admin
+def _api_maintenance_text_get(payload):
+    return (
+        json.dumps(Maintenance.get_text()),
+        200,
+        {"Content-Type": "application/json"},
+    )
+
+
+@app.route("/api/v3/maintenance/text/frontend", methods=["GET"])
+def _api_maintenance_text_frontend_get():
+    if Maintenance.enabled:
+        return (
+            json.dumps(Maintenance.get_text()),
+            200,
+            {"Content-Type": "application/json"},
+        )
+    else:
+        return (
+            json.dumps({}),
+            204,
+            {"Content-Type": "application/json"},
+        )
+
+
+@app.route("/api/v3/maintenance/text/enable/<set_custom>", methods=["PUT"])
+@is_admin
+def _api_maintenance_text_enable(payload, set_custom):
+    set_custom = set_custom == "true"
+    return (
+        json.dumps(Maintenance.enable_custom_text(set_custom)),
         200,
         {"Content-Type": "application/json"},
     )
