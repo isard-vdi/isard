25d4a00784906eb689856113e7c8f7e945d79000
Pau Abril <pauabril@duck.com>
2024-05-10 09:17:29 +0200

    fix(frontend): change video to default only if video is set to 'Only GPU'

--- frontend/src/components/domain/DomainBookables.vue
+++ frontend/src/components/domain/DomainBookables.vue
@@ -42,7 +42,7 @@ export default {
     const $store = context.root.$store
     const availableBookables = computed(() => $store.getters.getBookables)
     const domain = computed(() => $store.getters.getDomain)
-    const defaultVideos = computed(() => domain.value.hardware.videos.includes('default'))
+    const gpuVideos = computed(() => domain.value.hardware.videos.includes('none'))
     const vgpus = computed({
       get: () => $store.getters.getDomain.reservables.vgpus,
       set: (value) => {
@@ -53,7 +53,7 @@ export default {
 
     // When not selecting a GPU, set the video to default
     watch(vgpus, (newVal, prevVal) => {
-      if (vgpus.value[0] === 'None' && !defaultVideos.value) {
+      if (vgpus.value[0] === 'None' && gpuVideos.value) {
         ErrorUtils.showInfoMessage(context.root.$snotify, i18n.t('messages.info.video-default'), '', true, 5000)
         $store.dispatch('changeVideos', ['default'])
       }
