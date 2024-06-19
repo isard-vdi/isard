c6f56c9313eae3994daf4c953360aeb982b1af44
Miriam Melina Gamboa Valdez <melina.devel@gmail.com>
2024-06-19 11:47:04 +0200

    fix(authentication): saml middleware fixed

--- authentication/transport/http/request_metadata.go
+++ authentication/transport/http/request_metadata.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"context"
+	"net/http"
 	"strings"
 
 	"github.com/ogen-go/ogen/middleware"
@@ -11,16 +12,33 @@ type requestMetadataCtxKeyType string
 
 const requestMetadataRemoteAddrCtxKey requestMetadataCtxKeyType = "request_metadata_remote_addr"
 
-func RequestMetadata(
+func RequestMetadataOAS(
 	req middleware.Request,
 	next func(req middleware.Request) (middleware.Response, error),
 ) (middleware.Response, error) {
-	remoteAddr := req.Raw.RemoteAddr
-	if addr := req.Raw.Header.Get("X-Forwarded-For"); addr != "" {
-		remoteAddr = strings.TrimSpace(strings.Split(addr, ",")[0])
-	}
+	remoteAddr := extractRemoteAddr(req.Raw)
 
 	req.Context = context.WithValue(req.Context, requestMetadataRemoteAddrCtxKey, remoteAddr)
 
 	return next(req)
+}
+
+func requestMetadataHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		remoteAddr := extractRemoteAddr(r)
+
+		r = r.WithContext(context.WithValue(r.Context(), requestMetadataRemoteAddrCtxKey, remoteAddr))
+
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func extractRemoteAddr(r *http.Request) string {
+	remoteAddr := r.RemoteAddr
+	if addr := r.Header.Get("X-Forwarded-For"); addr != "" {
+		remoteAddr = strings.TrimSpace(strings.Split(addr, ",")[0])
+	}
+
+	return remoteAddr
 }
