b343e913c9187fb0fb90a3e617b64be566d3aa7a
Melina Gamboa <melina.devel@gmail.com>
2024-07-20 17:04:38 +0200

    feat(frontend): more tailwind and storybooks stories

--- frontend/src/components/ui/button/index.ts
+++ frontend/src/components/ui/button/index.ts
@@ -4,21 +4,45 @@ export { default as Button } from './Button.vue'
 
 export const buttonVariants = cva(
   `inline-flex items-center justify-center whitespace-nowrap
-    rounded-lg font-semibold
+    rounded-md font-semibold
     `,
 // ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50
   {
     variants: {
       variant: {
         primary: `
-          bg-primary text-primary-foreground 
-          hover:bg-primary-800
-          border-2 border-primary`,
-        secondaryGray: '',
-        secondaryColor: '',
-        tertiaryColor: '',
-        linkGray: '',
-        linkGrayColor: '',
+          bg-brand-700 text-base-white border-brand-700 border
+          hover:bg-brand-800 :hover:text-base-white
+          focused:ring-ring-brand-shadow-xs focused:rounded-xl
+          disabled:bg-gray-warm-100 disabled:text-gray-warm-400 disabled:border-gray-warm-200
+        `,
+        'secondary-gray': `
+          bg-base-white text-gray-warm-700 border-gray-warm-300 border
+          hover:bg-gray-warm-50 :hover:text-gray-warm-800
+          disabled:bg-base-white disabled:text-gray-warm-400 disabled:border-gray-warm-200
+        `,
+        'secondary-color': `
+          bg-base-white text-brand-700 border-brand-700 border
+          hover:bg-gray-warm-50 :hover:text-brand-700
+          disabled:bg-base-white disabled:text-gray-warm-400 disabled:border-gray-warm-200
+
+        `,
+        'tertiary-color': 
+        `
+          bg-base-white text-gray-warm-600 border-warning-300 border
+          hover:warning-300 :hover:text-gray-warm-50
+          disabled:bg-base-white disabled:text-gray-warm-400 disabled:border-gray-warm-200
+        `,
+        'link-gray':
+        `
+          text-gray-warm-600
+          disabled:text-gray-warm-400
+        `,
+        'link-color':
+        `
+          text-brand-700
+          disabled:text-gray-warm-400
+        `,
         // default: 'bg-primary text-primary-foreground hover:bg-primary/90',
 
         // destructive:
@@ -33,14 +57,9 @@ export const buttonVariants = cva(
       size: {
         sm: 'px-[12px] py-[8px] text-sm',
         md: 'px-[14px] py-[10px] text-sm',
-        lg: 'px-[16px] py-[10px] text-base',
-        xl: 'px-[18px] py-[12px] text-base',
-        xxl: 'px-[22px] py-[16px] text-lg',
-        // default: 'h-10 px-4 py-2',
-        // xs: 'h-7 rounded px-2',
-        // sm: 'h-9 rounded-md px-3',
-        // lg: 'h-11 rounded-md px-8',
-        // icon: 'h-10 w-10',
+        lg: 'px-[16px] py-[10px] text-md',
+        xl: 'px-[18px] py-[12px] text-md',
+        '2xl': 'px-[22px] py-[16px] text-lg',
       },
     },
     defaultVariants: {
--- frontend/src/stories/Button.stories.ts
+++ frontend/src/stories/Button.stories.ts
@@ -11,14 +11,14 @@ const meta = {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/design/FHPNZiT08g7iQysunKZkLm/N%C3%89FIX---ISARD-Design-system-Cliente?node-id=3466-410043&m=dev'
-      // url: 'https://www.figma.com/design/FHPNZiT08g7iQysunKZkLm/N%C3%89FIX---ISARD-Design-system-Cliente?node-id=1-1183&m=dev'
     },
   },
   argTypes: {
     // TODO: Use buttonVariants
-    variant: { control: 'select', options: ['primary', 'secondaryGray', 'linkColor'] },
+    variant: { control: 'select', options: ['primary', 'secondary-gray', 'secondary-color', 'tertiary-color', 'link-gray', 'link-color'] },
     // TODO: Use buttonVariants
-    size: { control: 'select', options: ['sm', 'md', 'lg', 'xl', 'xxl' ] },
+    size: { control: 'select', options: ['sm', 'md', 'lg', 'xl', '2xl' ] },
+    disabled: { control: 'boolean' },
   },
   render: (args) => ({
     components: { Button },
@@ -27,7 +27,7 @@ const meta = {
         args
       }
     },
-    template: `<Button :variant="args.variant" :size="args.size">Button</Button>`
+    template: `<Button :variant="args.variant" :size="args.size" :disabled="args.disabled">Button CTA</Button>`
   })
 } satisfies Meta<typeof Button>;
 
@@ -35,12 +35,38 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    variant: 'primary'
+  },
 };
 
-export const Secondary: Story = {
+export const SecondaryGray: Story = {
   args: {
-    variant: 'secondaryColor'
+    variant: 'secondary-gray'
+  },
+};
+
+export const SecondaryColor: Story = {
+  args: {
+    variant: 'secondary-color'
+  },
+};
+
+export const TertiaryColor: Story = {
+  args: {
+    variant: 'tertiary-color'
+  },
+};
+
+export const LinkGray: Story = {
+  args: {
+    variant: 'link-gray'
+  },
+};
+
+export const LinkColor: Story = {
+  args: {
+    variant: 'link-color'
   },
 };
 
@@ -70,6 +96,48 @@ export const ExtraLarge: Story = {
 
 export const ExtraExtraLarge: Story = {
   args: {
-    size: 'xxl'
+    size: '2xl'
+  },
+};
+
+export const PrimaryDisabled: Story = {
+  args: {
+    variant: 'primary',
+    disabled: true
+  },
+};
+
+export const SecondaryGrayDisabled: Story = {
+  args: {
+    variant: 'secondary-gray',
+    disabled: true
+  },
+};
+
+export const SecondaryColorDisabled: Story = {
+  args: {
+    variant: 'secondary-color',
+    disabled: true
+  },
+};
+
+export const TertiaryColorDisabled: Story = {
+  args: {
+    variant: 'tertiary-color',
+    disabled: true
+  },
+};
+
+export const LinkGrayDisabled: Story = {
+  args: {
+    variant: 'link-gray',
+    disabled: true
+  },
+};
+
+export const LinkColorDisabled: Story = {
+  args: {
+    variant: 'link-color',
+    disabled: true
   },
 };
