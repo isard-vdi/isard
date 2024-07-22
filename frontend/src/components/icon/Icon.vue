67cfbf89cc9b355a3c09339d3e2cdeb5248750ef
Néfix Estrada <nefixestrada@gmail.com>
2024-07-22 20:23:36 +0200

    feat(frontend): add Icon component

---
+++ frontend/src/components/icon/Icon.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed, type HTMLAttributes } from 'vue'
+import { Primitive, type PrimitiveProps } from 'radix-vue'
+import { cn } from '@/lib/utils'
+
+interface Props extends PrimitiveProps {
+  name: string
+  alt?: string
+  class?: HTMLAttributes['class']
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'img'
+})
+
+const url = computed(() => {
+  return new URL(`../../assets/icons/${props.name}.svg`, import.meta.url).href
+})
+</script>
+
+<template>
+  <Primitive
+    :src="url"
+    :alt="alt || props.name + ' icon'"
+    :as="as"
+    :as-child="asChild"
+    :class="cn('', props.class)"
+  ></Primitive>
+</template>
