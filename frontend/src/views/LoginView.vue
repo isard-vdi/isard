b27b4f18ea75afd6d7cb216604993a78f3b02627
Néfix Estrada <nefixestrada@gmail.com>
2024-08-14 18:06:20 +0200

    fix(frontend): fix login view

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-import LoginLayout from '@/layouts/LoginLayout.vue'
+import { LoginLayout } from '@/layouts/login'
+import { LoginProviderForm, LoginProviderExternal, Provider } from '@/components/login'
+import { Separator } from '@/components/ui/separator'
 </script>
 
 <template>
-  <LoginLayout version="v13.3.2" />
+  <LoginLayout version="v13.3.2">
+    <div class="flex flex-col space-y-4">
+      <LoginProviderForm />
+
+      <Separator />
+
+      <LoginProviderExternal :provider="Provider.Google" />
+      <LoginProviderExternal :provider="Provider.SAML" />
+    </div>
+  </LoginLayout>
 </template>
