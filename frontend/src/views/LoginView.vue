b3515306772264c8b67fbd68a13e735a9901b263
Néfix Estrada <nefixestrada@gmail.com>
2024-08-26 09:34:08 +0200

    fix(frontend): correct the login separator label i18n

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,16 +3,16 @@ import { LoginLayout } from '@/layouts/login'
 import { LoginProviderForm, LoginProviderExternal, Provider } from '@/components/login'
 import { Separator } from '@/components/ui/separator'
 import { useI18n } from 'vue-i18n'
-import { useRoute } from 'vue-router';
-import { computed } from 'vue';
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
 
 const { t } = useI18n()
-const route = useRoute();
+const route = useRoute()
 
 const isProviderValid = computed(() => {
-  const provider = route.params.provider as Provider;
-  return provider && Object.values(Provider).includes(provider);
-});
+  const provider = route.params.provider as Provider
+  return provider && Object.values(Provider).includes(provider)
+})
 </script>
 
 <template>
@@ -20,7 +20,7 @@ const isProviderValid = computed(() => {
     <div class="flex flex-col space-y-4">
       <LoginProviderForm />
 
-      <Separator  v-if="!isProviderValid" :label="t('components.login.separator')"/>
+      <Separator v-if="!isProviderValid" :label="t('views.login.separator')" />
 
       <LoginProviderExternal :provider="Provider.Google" />
       <LoginProviderExternal :provider="Provider.SAML" />
