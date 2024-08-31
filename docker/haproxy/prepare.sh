dcf5c3db2fe7d65451fe84b5fdcd3d2b75859727
root <josepmaria@isardvdi.com>
2024-08-31 21:40:29 +0200

    fix(haproxy): use mkpasswd instead of deprecated python crypt

--- docker/haproxy/prepare.sh
+++ docker/haproxy/prepare.sh
@@ -11,8 +11,7 @@ then
 fi
 cat "$tmp_dir"/*.cfg > /usr/local/etc/haproxy/haproxy.cfg
 
-# Set debug path password
-PASSWD=$(python3 -c 'import os,crypt,getpass; print(crypt.crypt(os.environ["WEBAPP_ADMIN_PWD"], crypt.mksalt(crypt.METHOD_SHA512)))')
+PASSWD=$(mkpasswd -m sha-512 $WEBAPP_ADMIN_PWD)
 sed -i "/^    user admin password/c\    user admin password $PASSWD" /usr/local/etc/haproxy/haproxy.cfg
 
 if [ -f /certs/custom-portal-chain.pem ]
