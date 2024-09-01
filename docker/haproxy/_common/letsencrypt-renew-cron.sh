6a2c335aeb9723264e188aab27eb45d3de34849d
root <josepmaria@isardvdi.com>
2024-09-01 07:51:26 +0200

    fix(haproxy): letsencrypt wont renew if manual

--- docker/haproxy/_common/letsencrypt-renew-cron.sh
+++ docker/haproxy/_common/letsencrypt-renew-cron.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+
+CONF_FILE="/etc/letsencrypt/renewal/$LETSENCRYPT_DOMAIN.conf"
+
+if [ -f "$CONF_FILE" ]; then
+    # If current certificate was generated with a different authenticator,
+    # we need to change it to standalone for the renewal or it won't work.
+    sed -i '/^pref_challs/d' "$CONF_FILE"
+    sed -i 's/^authenticator.*/authenticator = standalone/' "$CONF_FILE"
+fi
+
 certbot renew --http-01-port 8080 --cert-name $LETSENCRYPT_DOMAIN
