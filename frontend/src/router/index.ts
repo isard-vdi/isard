e66ce46f2009843e7e57c932561875c48f23b414
Pau Abril <pauabril@duck.com>
2024-09-02 11:45:25 +0200

    feat(frontend): add i18n for route titles

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { appTitle } from '@/shared/constants'
+import { i18n } from '@/i18n'
+
+const { t } = i18n.global
 
 const router = createRouter({
   history: createWebHistory(),
@@ -7,14 +11,20 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {title: t('router.titles.home')},
     },
     {
       path: '/login/:provider?/:category?',
       name: 'login',
-      component: () => import('../views/LoginView.vue')
+      component: () => import('../views/LoginView.vue'),
+      meta: {title: t('router.titles.login')},
     }
   ]
 })
 
 export default router
+
+router.beforeEach((to) => {
+  document.title = to.meta.title ? `${appTitle} - ${to.meta.title}` : appTitle
+})
---
+++ frontend/src/shared/constants.ts
@@ -0,0 +1,1 @@
+export const appTitle = 'IsardVDI'
