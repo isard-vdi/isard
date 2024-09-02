ae9552bd5fe756f6fed8b4bae0cc8be8f3e7048f
Néfix Estrada <nefixestrada@gmail.com>
2024-09-03 01:05:25 +0200

    fix(frontend): use i18n components correctly

--- frontend/src/components/login/LoginCategorySelect.vue
+++ frontend/src/components/login/LoginCategorySelect.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { useI18n, i18nT } from 'vue-i18n'
+import { useI18n } from 'vue-i18n'
 import { type CategorySelectToken } from '.'
 import {
   getToken as getAuthToken,
