020f005b9fe932717cd1f0cb74f7d30a056f11a5
darta <josepmaria@isardvdi.com>
2024-09-09 21:26:01 +0200

    fix(backupninja): check nfs mount

--- docker/backupninja/nfs_mount.sh
+++ docker/backupninja/nfs_mount.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mount -t nfs4 $BACKUP_NFS_SERVER:$BACKUP_NFS_FOLDER /backup
-if grep -qs '/backup ' /proc/mounts; then
+if grep -qs "$BACKUP_NFS_SERVER:$BACKUP_NFS_FOLDER" /proc/mounts; then
     echo "BACKUP NFS FOLDER MOUNTED: $BACKUP_NFS_SERVER:$BACKUP_NFS_FOLDER"
 else
     echo "ERROR!!! UNABLE TO MOUNT $BACKUP_NFS_SERVER:$BACKUP_NFS_FOLDER !!!"
