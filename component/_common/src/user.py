a0065e933573d317cf923fd59334c7b783819db3
darta <josepmaria@isardvdi.com>
2024-11-06 22:34:45 +0100

    fix(common): new user class

---
+++ component/_common/src/user.py
@@ -0,0 +1,32 @@
+#
+#   IsardVDI - Open Source KVM Virtual Desktops based on KVM Linux and dockers
+#   Copyright (C) 2024 Josep Maria Viñolas Auquer
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from .rethink_custom_base_factory import RethinkCustomBase
+
+
+class User(RethinkCustomBase):
+    """
+    Manage Domain Objects
+
+    Use constructor with keyword arguments to create new Domain Objects or
+    update an existing one using id keyword. Use constructor with id as
+    first argument to create an object representing an existing Domain Object.
+    """
+
+    _rdb_table = "users"
