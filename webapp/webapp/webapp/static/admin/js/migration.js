d51aaa0e3f8511fb3b528a3804da9530cb7a28a6
Miriam Melina Gamboa Valdez <melina.devel@gmail.com>
2026-05-28 11:11:25 +0200

    fix(webapp): users migrations display fixed

--- webapp/webapp/webapp/static/admin/js/migration.js
+++ webapp/webapp/webapp/static/admin/js/migration.js
@@ -174,7 +174,7 @@ function renderMigrationDataTable() {
             "url": `/api/v4/admin/item/user-migrations`,
             "type": 'GET'
         },
-        "sAjaxDataProp": "",
+        "sAjaxDataProp": "migrations",
         "language": {
             "loadingRecords": '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i><span class="sr-only">Loading...</span>'
         },
