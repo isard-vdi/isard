85109059a1a3d5ef0c589614a4ff234d9d3b272e
Néfix Estrada <nefixestrada@gmail.com>
2026-06-26 12:46:42 +0200

    build(deps): freeze git clones, submodules and binary downloads
    Make build inputs reproducible: drop 'git submodule update --remote' so
    submodules use their pinned gitlink commit, freeze the mdevctl clone to its
    v1.2.0 commit, and verify the yq binary download against a sha256 checksum.

--- docker/hypervisor/mdevctl_install.sh
+++ docker/hypervisor/mdevctl_install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 apk add --no-cache cargo uuidgen py3-docutils make git
 cd /opt
+# mdevctl v1.2.0
 git clone https://github.com/mdevctl/mdevctl
 cd mdevctl
+git checkout ec4b9a04ce15ad7dccef2dc99b20a53987eb16bc
 cargo build
 make install
 
