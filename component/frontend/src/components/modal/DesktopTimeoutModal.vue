6a71f2324bd2323b0f9e496b4bf0a6bc6ae8cecb
Pau Abril <pauabril@duck.com>
2026-09-04 12:33:51 +0200

    feat(frontend): add notification count to the document title

--- component/frontend/src/components/modal/DesktopTimeoutModal.vue
+++ component/frontend/src/components/modal/DesktopTimeoutModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, onUnmounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useMutation } from '@tanstack/vue-query'
 
@@ -39,6 +39,8 @@ const onExtend = () => {
 const onUpdateOpen = (value: boolean) => {
   if (!value) clearDesktopTimeout()
 }
+
+onUnmounted(clearDesktopTimeout)
 </script>
 
 <template>
